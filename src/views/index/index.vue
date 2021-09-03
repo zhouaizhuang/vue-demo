ffba07ec5145021f80dfa34a9fe12b6fee4bf233
周爱桩 <zhouaizhuang000@163.com>
2021-09-03 15:29:04 +0800

    特殊字体

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,10 +1,18 @@
 
 <template>
-  <div>
-    <div class="f xs ac p30">
-      <router-link class="db w20 g42b983 pb20" to="/carousel">keyFrames无缝滚动动画</router-link>
-      <router-link class="db w20 g42b983 pb20" to="/">数字滚动效果</router-link>
-      <router-link class="db w20 g42b983 pb20" to="/">呼吸效果</router-link>
+  <div class="p30">
+    <div class="fs30 b mb20 tc">动画效果</div>
+    <div class="f xs ac rw ">
+      <router-link class="db w20 g42b983 pb30" to="/carousel">keyFrames无缝滚动动画</router-link>
+      <router-link class="db w20 g42b983 pb30" to="/">数字滚动效果</router-link>
+      <router-link class="db w20 g42b983 pb30" to="/">呼吸效果</router-link>
+      <!-- <router-link class="db w20 g42b983 pb30" to="/">呼吸效果</router-link>
+      <router-link class="db w20 g42b983 pb30" to="/">呼吸效果</router-link>
+      <router-link class="db w20 g42b983 pb30" to="/">呼吸效果</router-link> -->
+    </div>
+    <div class="fs30 b mt30 mb20 tc">其他demo</div>
+    <div class="f xs ac rw">
+      <router-link class="db w20 g42b983 pb30" to="/specialFont">特殊字体支持</router-link>
     </div>
   </div>
 </template>

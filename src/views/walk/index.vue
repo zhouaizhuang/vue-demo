4784ed8c3b6a2d1d83ff30165c38d6e366883185
周爱桩 <840043348@qq.com>
2021-09-24 08:46:43 +0800

    增加小人行走动画

---
+++ src/views/walk/index.vue
@@ -0,0 +1,41 @@
+<!--图片帧动画-->
+<!--在线将图片制作为精灵图网址：https://www.toptal.com/developers/css/sprite-generator/-->
+<template>
+  <div>
+    <div class="wrapper" :style="styleObj"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Walk',
+  data(){
+    return {
+      styleObj:{},
+      pos: 0,
+    }
+  },
+  methods: {
+ 
+  },
+  created(){
+    setInterval(() => {
+      if(this.pos < 8) {
+        this.styleObj = { 'background-position': `${-this.pos*184}px 0px` }
+        this.pos++
+      } else {
+        this.pos = 0
+      }
+    }, 100)
+  }
+}
+</script>
+<style>
+.wrapper {
+  width: 184px;
+  height: 325px;
+  background-image: url('test.jpg');
+  background-size: 800% 100%; /**因为有8张图，所有横向宽度为8倍当前宽度 */
+  background-repeat: no-repeat;
+  margin: 100px auto;
+}
+</style>

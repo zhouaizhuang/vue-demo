18822bf4e7e8fe0706441aa31eecd21fc93406cd
周爱桩 <840043348@qq.com>
2021-10-30 12:00:31 +0800

    完善一些注释

--- src/views/css3Ani/arrow/index.vue
+++ src/views/css3Ani/arrow/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="rel" style="height:300px;width:700px;margin:100px auto;">
-    <div style="width: 80px;height:80px;" class="rds50 bdc f ac xc">位置1</div>
-    <div style="width: 80px;height:80px;bottom:0px;right:0px;" class="abs rds50 bdc f ac xc">位置2</div>
-    <div class="abs dash-box">
-      <div class="aniLen ">
-        <div class="bdLight"></div>
-        <img src="./arraw_light.png" alt="" class="abs l50 t0 arrow" :style="{'transform': 'translate(-50%, -53%) rotate(-4deg)'}">
+  <div>
+    <div class="fs24 tc mt50">备注：<span class="gf10000">动画开始时间</span>可以设置<span class="gf10000">为负数</span>。这样能保证一进入页面动画已经在播放中</div>
+    <div class="fs24 tc">在一些场景需要有时间差的开始动画，但是一进入页面又希望他们都在动起来了，就非常有用</div>
+    <div class="rel" style="height:300px;width:700px;margin:100px auto;">
+      <div style="width: 80px;height:80px;" class="rds50 bdc f ac xc">位置1</div>
+      <div style="width: 80px;height:80px;bottom:0px;right:0px;" class="abs rds50 bdc f ac xc">位置2</div>
+      <div class="abs dash-box">
+        <div class="aniLen ">
+          <div class="bdLight"></div>
+          <img src="./arraw_light.png" alt="" class="abs l50 t0 arrow" :style="{'transform': 'translate(-50%, -53%) rotate(-4deg)'}">
+        </div>
       </div>
     </div>
   </div>
@@ -43,6 +47,9 @@ export default {
 }
 .aniLen{
   transform-origin:left top;
-  animation: len 1.5s ease 0s 1 forwards;
+  /**当播放动画是负数的时候。提前开始播放动画。页面效果就变成一进
+  *入页面动画已经在播放中了。有的尝尽需要错落播放，但是有希望一进入页面就动起来，就非常有用 
+  */
+  animation: len 1.5s ease -.3s 1 forwards; 
 }
 </style>
--- src/views/css3Ani/countDown/index.vue
+++ src/views/css3Ani/countDown/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="fs30" style="margin:200px auto;width:500px;">{{timeStr}}</div>
+    <div class="fs22 mt50 tc">备注：h5限时答题，限时活动场景中非常常见的倒计时功能</div>
+    <div class="fs22 tc">如果不想为了一个倒计时而在项目中引入UI库，那么这个改造一下就很适合使用</div>
+    <div class="fs30" style="margin:100px auto;width:500px;">{{timeStr}}</div>
   </div>
 </template>
 <script>
@@ -18,7 +20,7 @@ export default {
       const [now, end] = [new Date().getTime(), new Date(time).getTime()]
       const leftMs = end - now //时间差
       if(leftMs > 0) {
-        const { d, h, m, s, ms } = sec2Dhs(leftMs)
+        const { d, h, m, s, ms } = sec2Dhs(leftMs) // 获取剩余的 天  时  分 秒 毫秒
         this.timeStr = `${d}天${h}时${m}分${s}秒${ms}毫秒`
       } else {
         this.timeStr = '已截止'
@@ -32,7 +34,7 @@ export default {
       if(dates <= 0) {
         clearInterval(this.timeId)
       }
-    },80) 
+    },60) 
   },
   beforeDestroy(){
     clearInterval(this.timeId)

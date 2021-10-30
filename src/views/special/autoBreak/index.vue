d580f6c1b50904487da291dba31235f025574a5f
周爱桩 <840043348@qq.com>
2021-10-30 13:11:12 +0800

    增加fit-content的高级用法

--- src/views/special/autoBreak/index.vue
+++ src/views/special/autoBreak/index.vue
@@ -1,17 +1,21 @@
 
 <template>
   <div>
-    <div class="fs30 mt50 bdc auto" style="width:400px;">
+    <div class="fs30 mt50 bdc auto g42b983" style="width:400px;">
       <div class="wfc mauto">不足一行自动居中</div>
     </div>
-    <div class="fs30 mt50 bdc auto" style="width:400px;">
+    <div class="fs30 mt50 bdc auto g42b983" style="width:400px;">
       <div class="wfc mauto">文字非常多的时超过了一行，那么就左对齐了</div>
     </div>
-    <div class="fs30 mt50 bdc auto" style="width:400px;">
+    <div class="fs30 mt50 bdc auto g1890ff" style="width:400px;">
       <div class="nowrap">这是一行文字溢出隐藏效果，后面的文字即将被隐藏</div>
     </div>
-    <div class="fs30 mt50 bdc auto" style="width:400px;">
+    <div class="fs30 mt50 bdc auto g1890ff" style="width:400px;">
       <div class="nowrap2">这是超出两行，溢出隐藏效果，后面的文字即将被隐藏为省略号</div>
+    </div>
+    <div class="fs22 tc mt50 gf10000">fit-content的高级运用。给未知宽高的元素赋予确定的尺寸。从而能水平垂直居中</div>
+    <div class="bdc rel auto gf10000" style="width:200px;height:200px;">
+      <div class="wfc hfc trbl0 abs mauto">ans</div>
     </div>
   </div>
 </template>

b49b02adc2a34215452c7191f56e68d618d7c150
周爱桩 <840043348@qq.com>
2021-10-31 19:51:48 +0800

    变更块

--- src/router/module/special.js
+++ src/router/module/special.js
@@ -2,21 +2,21 @@ export const special = [
   {
     path: '/specialFont',
     name: 'specialFont',
-    component: () => import(/* webpackChunkName: "other" */ '../../views/special/specialFont/index.vue')
+    component: () => import(/* webpackChunkName: "special" */ '../../views/special/specialFont/index.vue')
   },
   {
     path: '/linearText',
     name: 'linearText',
-    component: () => import(/* webpackChunkName: "other" */ '../../views/special/linearText/index.vue')
+    component: () => import(/* webpackChunkName: "special" */ '../../views/special/linearText/index.vue')
   },
   {
     path: '/autoBreak',
     name: 'autoBreak',
-    component: () => import(/* webpackChunkName: "other" */ '../../views/special/autoBreak/index.vue')
+    component: () => import(/* webpackChunkName: "special" */ '../../views/special/autoBreak/index.vue')
   },
   {
     path: '/filter',
     name: 'filter',
-    component: () => import(/* webpackChunkName: "other" */ '../../views/special/filter/index.vue')
+    component: () => import(/* webpackChunkName: "special" */ '../../views/special/filter/index.vue')
   },
 ]

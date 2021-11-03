f4285a98adb32f64a6920c5870bfce6f42e6f0c9
周爱桩 <840043348@qq.com>
2021-11-03 22:25:36 +0800

    不规则运动

--- src/router/module/svgAni.js
+++ src/router/module/svgAni.js
@@ -5,6 +5,11 @@ export const svgAni = [
     component: () => import(/* webpackChunkName: "svgAni" */ '../../views/svgAni/moveText/index.vue')
   },
   {
+    path: '/moveSvg',
+    name: 'moveSvg',
+    component: () => import(/* webpackChunkName: "svgAni" */ '../../views/svgAni/moveSvg/index.vue')
+  },
+  {
     path: '/stickPop',
     name: 'stickPop',
     component: () => import(/* webpackChunkName: "svgAni" */ '../../views/svgAni/stickPop/index.vue')
---
+++ src/views/svgAni/moveSvg/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div >
+    <div class="rel">
+      <img class="abs run1" style="width:40px;height:40px;" src="https://upload.qianfanyun.com/yjyn/ylyn_life_bg.png">
+      <svg width="280" height="150" viewBox="0 0 280 150">
+        <path d="M10,80 q100,120 120,20 q140,-50 160,0" stroke="#cd0000" stroke-width="2" fill="none" />
+      </svg>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'moveSvg',
+  data(){
+    return {
+    }
+  },
+  methods: {
+   
+  },
+  created(){
+  
+  },
+}
+</script>
+<style>
+.run1 {
+  offset-anchor:0 0;
+  offset-path: path("M10,80 q100,120 120,20 q140,-50 160,0");
+  animation: motion 3s linear infinite;
+}
+@keyframes motion {
+  100% { offset-distance: 100%;}
+}
+</style>

874132d148bd598eff774e9ccb72a7ed2d9dbc41
周爱桩 <840043348@qq.com>
2021-11-12 16:25:37 +0800

    箭头指向函数适配移动端

--- src/views/css3Ani/arrow/index.vue
+++ src/views/css3Ani/arrow/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <div class="fs24 tc mt50">备注：<span class="gf10000">动画开始时间</span>可以设置<span class="gf10000">为负数</span>。这样能保证一进入页面动画已经在播放中</div>
-    <div class="fs24 tc">在一些场景需要有时间差的开始动画，但是一进入页面又希望他们都在动起来了，就非常有用</div>
-    <div class="rel" style="height:300px;width:700px;margin:100px auto;">
+  <div style="width:350px;" class="auto">
+    <div class="fs16 tc mt50">备注：<span class="gf10000">动画开始时间</span>可以设置<span class="gf10000">为负数</span>。这样能保证一进入页面动画已经在播放中</div>
+    <div class="fs16 tc">在一些场景需要有时间差的开始动画，但是一进入页面又希望他们都在动起来了，就非常有用</div>
+    <div class="rel" style="height:300px;width:350px;margin:100px auto;">
       <div style="width: 80px;height:80px;" class="rds50 bdc f ac xc">位置1</div>
       <div style="width: 80px;height:80px;bottom:0px;right:0px;" class="abs rds50 bdc f ac xc">位置2</div>
       <div class="abs dash-box">
         <div class="aniLen">
           <div class="bdLight"></div>
-          <img src="./arraw_light.png" alt="" class="abs l50 t0 arrow" :style="{'transform': 'translate(-50%, -48%) rotate(-4deg)'}">
+          <img src="./arraw_light.png" alt="" class="abs l50 t0" :style="{'transform': 'translate(-50%, -48%) rotate(-4deg)', width:'91px',height:'78px'}">
         </div>
       </div>
     </div>
@@ -16,7 +16,7 @@
 </template>
 <script>
 export default {
-  name: 'Home',
+  name: 'arrow',
   data(){
     return {
 
@@ -33,13 +33,13 @@ export default {
 .dash-box{
   transform-origin:left top; /**基准旋转位置默认是中心的， 这里改为左上角 */
   height: 90px;
-  left:100px;
-  top:18%;
-  width:530px;
-  transform:rotate(20deg);
+  left:90px;
+  top:25%;
+  width:220px;
+  transform:rotate(40deg);
 }
 .bdLight{
-  border-top:7px dashed #2DC4FE;
+  border-top:4px dashed #2DC4FE;
 }
 @keyframes len {
   0%{transform: scale(0, 1);}

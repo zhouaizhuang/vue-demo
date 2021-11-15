18ac2485eec0350c13fa26ab4e718648d11566b2
周爱桩 <840043348@qq.com>
2021-11-15 16:13:39 +0800

    测试attrs传值

--- src/views/css3Ani/breath/index.vue
+++ src/views/css3Ani/breath/index.vue
@@ -2,17 +2,22 @@
 <template>
   <div class="f xc ac h100vh">
     <img src="./free_contract.png" class="breath-btn" style="width:400px;height:58px;" />
+    <!-- <HelloWorld :msg="'123'" @sayName="sayName"></HelloWorld> -->
   </div>
 </template>
 <script>
+// import HelloWorld from '../../../components/HelloWorld.vue'
 export default {
+  // components: { HelloWorld },
   name: 'breath',
   data(){
     return {
     }
   },
   methods: {
-   
+    // sayName(){
+    //   console.log('myName')
+    // }
   },
   created(){
   

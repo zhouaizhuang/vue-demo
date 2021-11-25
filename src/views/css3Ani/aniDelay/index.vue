b64b6eca0210c656753971926efbacd512f11a26
周爱桩 <840043348@qq.com>
2021-11-25 14:29:32 +0800

    样式冲突修复

--- src/views/css3Ani/aniDelay/index.vue
+++ src/views/css3Ani/aniDelay/index.vue
@@ -2,16 +2,16 @@
   <div style="width:350px;" class="auto bg2 h100vh pt100">
     <div class="f xc ac">
       <div class="rel rds50 mr50" style="width:30px;height:30px;background-color:#7ef9ff;">
-        <div class="abs w100 h100 wave rds50" style="background: inherit;animation-delay: -0.2s;" />
+        <div class="abs w100 h100 wave-scale rds50" style="background: inherit;animation-delay: -0.2s;" />
       </div>
       <div class="rel rds50 mr50" style="width:30px;height:30px;background-color:#89cff0;">
-        <div class="abs w100 h100 wave rds50" style="background: inherit;animation-delay: -0.1s;" />
+        <div class="abs w100 h100 wave-scale rds50" style="background: inherit;animation-delay: -0.1s;" />
       </div>
       <div class="rel rds50 mr50" style="width:30px;height:30px;background-color:#4682b4;">
-        <div class="abs w100 h100 wave rds50" style="background: inherit;animation-delay: 0s;" />
+        <div class="abs w100 h100 wave-scale rds50" style="background: inherit;animation-delay: 0s;" />
       </div>
       <div class="rel rds50" style="width:30px;height:30px;background-color:#0f52ba;">
-        <div class="abs w100 h100 wave rds50" style="background: inherit;animation-delay: 0.1s;" />
+        <div class="abs w100 h100 wave-scale rds50" style="background: inherit;animation-delay: 0.1s;" />
       </div>
     </div>
   </div>
@@ -32,7 +32,7 @@ export default {
 }
 </script>
 <style scoped>
-.wave{
+.wave-scale{
   animation: wave1 2s ease-out infinite;
 }
 @keyframes wave1 {

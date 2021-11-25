d3fb78cd1dc1072957a9de271b4815e1df1c554b
周爱桩 <840043348@qq.com>
2021-11-25 16:32:35 +0800

    增加vue.config.js的默认配置

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     //   '/': {
     //     target:'http://127.0.0.1:8080',
     //     changeOrigin: true  // node帮助我们用中间件实现了
+    //     secure: false,
     //   }
     // }
   }

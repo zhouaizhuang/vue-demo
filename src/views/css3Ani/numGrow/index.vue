1ce7eb8428e8997a4790ac17b99247763445e718
周爱桩 <840043348@qq.com>
2021-12-01 21:53:03 +0800

    增加内联模式

--- src/views/css3Ani/numGrow/index.vue
+++ src/views/css3Ani/numGrow/index.vue
@@ -1,14 +1,27 @@
 
 <template>
-  <div style="margin: 200px auto;width:300px;">
+  <div class="ovh" style="margin: 100px auto;width:300px;">
     <div class="mb30">每隔3秒滚动一次。可以自定义滚动数据</div>
     <div class="mb30">移动端和PC端均可使用</div>
-    <div class="fs24 f as rel bdc ovh" style="height:40px;transform: translateY(2px)">
-      <div class="f xc rw" v-for="(item, index) in playerArr" :key="index">
-        <div :style="{width:'20px', transition: 'all ease 1.5s', transform: `translateY(${item * -40}px)`}">
-          <div class="w100 f ac xc" style="height:40px;" v-for="item in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="item">{{item}}</div>
+    <!--1、块级模式-->
+    <div class="mt30 g1890ff">块级模式:</div>
+    <div class="fs24 f as rel bdc ovh" style="height:40px">
+      <div class="f xc rw" v-for="(item, index) in playerArr" :key="index" style="width:20px;">
+        <div class="w100" :style="{transition: 'all ease 1.5s', transform: `translateY(${item * -40}px)`}">
+          <div class="f ac xc" style="height:40px;" v-for="item in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="item">{{item}}</div>
         </div>
       </div>
+    </div>
+    <!--2、内联模式-->
+    <div class="mt30 g1890ff">内联模式:</div>
+    <div class="f ac">
+      <div class="fs24 f rel ovh" style="height:40px;">
+        <div class="f rw" v-for="(item, index) in playerArr" :key="index" style="width:20px;">
+          <div class="w100" :style="{transition: 'all ease 1.5s', transform: `translateY(${item * -40}px)`}">
+            <div class="f ac xc" style="height:40px;" v-for="item in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="item">{{item}}</div>
+          </div>
+        </div>
+      </div>人
     </div>
   </div>
 </template>
@@ -18,7 +31,7 @@ export default {
   name: 'numGrow',
   data(){
     return {
-      playerArr: [1,2,4,3,2,3,0,8,3,7,4,6]
+      playerArr: [0,0,0,0,0,0,0,0,0]
     }
   },
   methods: {
@@ -34,6 +47,9 @@ export default {
       tmp += Math.floor(random(1, 100000))
       this.playerArr = String(tmp).split('')
     }, 3000)
+    // setTimeout(() => {
+    //   this.playerArr = [1,3,5,6,8,4,3,7,3]
+    // }, 1000)
   },
   beforeDestroy(){
     clearInterval(this.timeId)

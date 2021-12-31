658b2fc3a754bc05dfcadd93435efd0c3854e7dc
周爱桩 <840043348@qq.com>
2021-12-31 12:05:58 +0800

    新增v-defaulyImg指令。图片未加载出来之前显示默认图片

---
+++ src/directives/modules/defaultImg.js
@@ -0,0 +1,28 @@
+/**
+ * 默认图片 图片暂时没加载出来之前显示的默认图片、或者随机颜色
+ * @param {String} - 图片链接
+ * @param {Array | String} binding.value - [String, String] | String
+ * 使用方法1、配置默认的图片： <img v-for="(item, index) in imgArr" :key="index" v-defaultImg="[item, 'https://gw.alicdn.com/tps/i1/TB147JCLFXXXXc1XVXXxGsw1VXX-112-168.png']">
+ * 使用方法2、不配置图片，则显示随机颜色块：<img v-for="(item, index) in imgArr" :key="index" v-defaultImg="item">
+ */
+export const defaultImg = {
+  inserted: function (el, binding) {
+    let [src, defaultImg]  = ['', '']
+    const bindVal = binding.value
+    if (typeof bindVal == 'string') { // 值传入函数的话。那么监听事件就是默认值click，延迟事件就是默认的500ms
+      src = bindVal
+      // 如果没传入默认图片，则图片没加载出来之前显示的随机颜色块
+      let color = Math.floor(Math.random() * 1000000)
+      el.style.backgroundColor = `#${color}`
+    } else {
+      [src, defaultImg = ''] = bindVal
+      console.log(defaultImg)
+      if(defaultImg) { el.src = defaultImg }
+    }
+    let img = new Image()
+    img.src = src
+    img.onload = function () {
+      el.src= src
+    }
+  }
+}
---
+++ src/views/vueAdvance/vDefaultImg/index.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div class="auto fs20r">
+    <!--图片没加载出来之前，先显示一个颜色块-->
+    <img v-for="(item, index) in imgArr" :key="index" v-defaultImg="[item, 'https://gw.alicdn.com/tps/i1/TB147JCLFXXXXc1XVXXxGsw1VXX-112-168.png']" style="width:300px;height:50px;background-size:100% 100%;" class="db auto mb50r img-fill">
+  </div>
+</template>
+<script>
+export default {
+  name: 'vLazyLoad',
+  data(){
+    return {
+      imgArr: [
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_design_bnt.png', 'https://upload.qianfanyun.com/jj_design_bnt.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+        'https://upload.qianfanyun.com/jj_case_03_01.png', 'https://upload.qianfanyun.com/jj_case_01.png','https://upload.qianfanyun.com/jj_design_bnt.png',
+      ]
+    }
+  },
+  methods:{
+    
+  },
+  created(){
+  }
+}
+</script>
+<style scoped>
+
+</style>

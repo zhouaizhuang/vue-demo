579790223852affc6edc2d9170119f73d8be75d2
周爱桩 <840043348@qq.com>
2021-12-31 12:18:55 +0800

    长按指令优化

--- src/directives/modules/longpress.js
+++ src/directives/modules/longpress.js
@@ -1,31 +1,33 @@
+/**
+ * 长按指令 长按一定时间后触发事件。默认是1s
+ * @param {Function} fn - 执行事件
+ * @param {?Number|1000} time - 长按时间
+ * @param {Array} binding.value - [fn, time]
+ * 使用方法1、直接使用： <div v-longpress="longpress1">长按1s</div>
+ * 使用方法2、配置事件，长按触发时间1s： <div v-longpress="[longpress1, 2000]">长按2s</div>
+ */
 export const longpress = {
   bind: function (el, binding, vNode) {
-    if (typeof binding.value !== 'function') {
-      throw 'callback must be a function'
+    let fn, time = 1000
+    if (typeof binding.value == 'function') { // 值传入函数的话。那么监听事件就是默认值click，延迟事件就是默认的500ms
+      fn = binding.value
+    } else {
+      [fn, time = 1000] = binding.value
     }
     // 定义变量
     let pressTimer = null
     // 创建计时器（ 2秒后执行函数 ）
-    let start = (e) => {
-      if (e.type === 'click' && e.button !== 0) {
-        return
-      }
+    let start = () => {
       if (pressTimer === null) {
-        pressTimer = setTimeout(() => {
-          handler()
-        }, 1000)
+        pressTimer = setTimeout(() => fn(), time)
       }
     }
     // 取消计时器
-    let cancel = (e) => {
+    let cancel = () => {
       if (pressTimer !== null) {
         clearTimeout(pressTimer)
         pressTimer = null
       }
-    }
-    // 运行函数
-    const handler = (e) => {
-      binding.value(e)
     }
     // 添加事件监听器
     el.addEventListener('mousedown', start)

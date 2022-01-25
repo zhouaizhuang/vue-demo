d1ed5b6ec5f43b2f06db7a9a072e0dd8d0262992
周爱桩 <840043348@qq.com>
2022-01-25 11:20:56 +0800

    优化父子组件传值

--- src/views/vueAdvance/propsEmit/father.vue
+++ src/views/vueAdvance/propsEmit/father.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="auto fs20r h100vh fs50r">
     父组件
-    <child :msg="'我是来自父组件的数据'" :info="{name:'小红', school: '北京大学', age: 18}" :state="'毕业'" @sayHello="sayHello"/>
+    <child :msg="'我是来自父组件的数据'" :info="{name:'小红', school: '北京大学', age: 18}" :state="state" @sayHello="sayHello"/>
   </div>
 </template>
 <script>
@@ -14,13 +14,14 @@ export default {
   },
   data(){
     return {
-      sayHello(){
-        alert('hello，执行了父组件传入的函数')
-      }
+      state: '毕业'
     }
   },
   methods:{
-    
+    sayHello(){
+      this.state = '入学'
+      alert('hello，执行了父组件传入的函数')
+    }
   },
   created(){
   }

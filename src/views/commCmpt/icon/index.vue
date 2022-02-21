89e5f6ebf1b4f7f5408ab73da0449ef6b438877c
周爱桩 <840043348@qq.com>
2022-02-21 14:38:21 +0800

    使用类样式，方便未来复制

--- src/views/commCmpt/icon/index.vue
+++ src/views/commCmpt/icon/index.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="fixed trbl0" style="background: rgba(0,0,0,.8);">
-    <div class="successIcon" />
-    <div class="errorIcon" />
-    <div class="closeIcon" />
+    <!-- 钩 -->
+    <div class="abs rds50 l10 t10 tr-45" style="width:80px;height:80px;border:4px solid #fff;background-color: #7abd3b;" >
+      <div class="w50 h25 rds5 abs l50 t50 " style="transform: translate(-40%,-60%);border: 7px solid #fff;border-top:none;border-right:none;background: transparent;"></div>
+    </div>
+    <!-- 叉 -->
+    <div class="abs rds50 l40 t10 tr-45" style="width:80px;height:80px;border:4px solid #fff;background-color: #fd2f2d;">
+      <div class="w85 h8 abs l50 t50 txy-50 ty-50 rds5 bgf" style="transform-origin:center center;"></div>
+      <div class="w8 h85 abs l50 t50 txy-50 bgf rds5" style="transform-origin:center center;"></div>
+    </div>
+    <!-- 叉 -->
+    <div class="abs rds50 r30 t10 tr-45" style="width:80px;height:80px;border: 4px solid #fff;">
+      <div class="w85 h7 abs l50 t50 txy-50 rds5 bgf" style="transform-origin:center center;"></div>
+      <div class="w7 h85 abs l50 t50 txy-50 rds5 bgf" style="transform-origin:center center;"></div>
+    </div>
   </div>
 </template>
 <script>
@@ -21,101 +32,4 @@ export default {
 }
 </script>
 <style>
-/**正确 ***/
-.successIcon{
-  position: absolute;
-  border-radius: 50%;
-  left:10%;
-  top:10%;
-  width: 80px;
-  height:80px;
-  border: 4px solid #fff;
-  background-color: #7abd3b;
-  transform: rotate(-45deg);
-}
-.successIcon::after{
-  content: '';
-  width: 50%;
-  height: 25%;
-  border-radius: 5px;
-  transform: translate(-40%, -60%);
-  border: 7px solid #fff;
-  border-top: none;
-  border-right: none;
-  background: transparent;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-}
-/*错误答案图标*/
-.errorIcon{
-  position: absolute;
-  border-radius: 50%;
-  left:40%;
-  top:10%;
-  width: 80px;
-  height:80px;
-  border: 4px solid #fff;
-  background-color: #fd2f2d;
-  transform: rotate(-45deg);
-}
-.errorIcon::before{
-  content: '';
-  width: 85%;
-  height: 8%;
-  position: absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%, -50%);
-  transform-origin: center center;
-  background-color: #fff;
-  border-radius: 5px;
-}
-.errorIcon::after{
-  content: '';
-  width: 8%;
-  height: 85%;
-  position: absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%, -50%);
-  transform-origin: center center;
-  background-color: #fff;
-  border-radius: 5px;
-}
-/**关闭 */
-.closeIcon{
-  position: absolute;
-  border-radius: 50%;
-  left:70%;
-  top:10%;
-  width: 80px;
-  height:80px;
-  border: 4px solid #fff;
-  transform: rotate(-45deg);
-}
-.closeIcon::before{
-  content: '';
-  width: 85%;
-  height: 8%;
-  position: absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%, -50%);
-  transform-origin: center center;
-  background-color: #fff;
-  border-radius: 5px;
-}
-.closeIcon::after{
-  content: '';
-  width: 8%;
-  height: 85%;
-  position: absolute;
-  left:50%;
-  top:50%;
-  transform: translate(-50%, -50%);
-  transform-origin: center center;
-  background-color: #fff;
-  border-radius: 5px;
-}
 </style>

d6a18bc95ef5f354b38e4778146649b6b9463a88
周爱桩 <840043348@qq.com>
2022-03-04 14:24:26 +0800

    格式化代码格式

--- src/views/vueAdvance/propsEmit/child.vue
+++ src/views/vueAdvance/propsEmit/child.vue
@@ -1,13 +1,14 @@
-
 <template>
   <div class="ml30r mr30r pt30r pb30r pl30r pr30r bdc fs20r mt30r">
     <div class="fs30r b mb20r">子组件</div>
-    <div class="mb20r">{{msg}}</div>
-    <div class="mb20r">姓名: {{info.name}}</div>
-    <div class="mb20r">学校: {{info.school}}</div>
-    <div class="mb20r">年龄: {{info.age}}</div>
-    <div class="mb20r">状态: {{state}}</div>
-    <div @click="doClick" class="pl20r pb20r pt20r pl30r pr30r bg1890ff gf w40 rds20r f ac xc fs28r">执行父组件事件</div>
+    <div class="mb20r">{{ msg }}</div>
+    <div class="mb20r">姓名: {{ info.name }}</div>
+    <div class="mb20r">学校: {{ info.school }}</div>
+    <div class="mb20r">年龄: {{ info.age }}</div>
+    <div class="mb20r">状态: {{ state }}</div>
+    <div @click="doClick" class="pl20r pb20r pt20r pl30r pr30r bg1890ff gf w40 rds20r f ac xc fs28r">
+      执行父组件事件
+    </div>
   </div>
 </template>
 <script>
@@ -21,29 +22,27 @@ export default {
     },
     info: {
       type: Object, // 数据类型
-      default: function () { // 默认值
-        return { name: '小明', school: '清华大学', age: 18}
+      default: function() {
+        // 默认值
+        return { name: '小明', school: '清华大学', age: 18 }
       }
     },
     state: {
-      validator: function (val) { // 可以写个函数做校验
+      validator: function(val) {
+        // 可以写个函数做校验
         return ['毕业', '在读', '肄业'].includes(val)
       }
     }
   },
-  data(){
-    return {
-    }
+  data() {
+    return {}
   },
-  methods:{
-    doClick(){
+  methods: {
+    doClick() {
       this.$emit('sayHello', '入学')
     }
   },
-  created(){
-  }
+  created() {}
 }
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>

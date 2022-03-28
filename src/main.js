c5b00413f028e9462e9509cd3349ed2761062aee
周爱桩 <840043348@qq.com>
2022-03-28 13:32:08 +0800

    降低函数职责

--- src/main.js
+++ src/main.js
@@ -8,7 +8,46 @@ Vue.config.productionTip = false
 // 使用自定义指令
 import Directives from "./directives/index.js"
 Vue.use(Directives)
-// import {url2JSON, JSON2url} from "./common"
+import {ms2Dhs, url2JSON, JSON2url} from "./common"
+console.log(ms2Dhs('dd天hh小时mm分钟ss秒', 62.5e3))
+console.log('1');
+
+
+setTimeout(function() {
+  console.log('2');
+  process.nextTick(function() {
+    console.log('3');
+  })
+  new Promise(function(resolve) {
+    console.log('4');
+    resolve();
+  }).then(function() {
+    console.log('5')
+  })
+})
+// process.nextTick(function() {
+//     console.log('6');
+// })
+// new Promise(function(resolve) {
+//     console.log('7');
+//     resolve();
+// }).then(function() {
+//     console.log('8')
+// })
+
+// setTimeout(function() {
+//     console.log('9');
+//     process.nextTick(function() {
+//         console.log('10');
+//     })
+//     new Promise(function(resolve) {
+//         console.log('11');
+//         resolve();
+//     }).then(function() {
+//         console.log('12')
+//     })
+// })
+
 new Vue({
   router,
   store,

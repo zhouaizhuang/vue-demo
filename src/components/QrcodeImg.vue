b5b6a822de38c0ed90cfa225e016f77a9b1305d5
周爱桩 <840043348@qq.com>
2022-05-22 19:45:27 +0800

    增加下载功能

--- src/components/QrcodeImg.vue
+++ src/components/QrcodeImg.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <div ref="qrcode"></div>
+    <div id="qrcode" ref="qrcode"></div>
+    <div class="tc mt30">
+      <Button type="primary" @click="saveImg">下载二维码</Button>
+    </div>
   </div>
 </template>
 <script>
@@ -19,14 +22,42 @@ export default {
     height: {
       type: String,
       default: "100"
+    },
+    fileName: {
+      type: String,
+      default: "qrcode"
     }
   },
-  components:{},
+  components:{
+  },
   data(){
     return {
     }
   },
   methods:{
+    // 下载图片
+    saveImg(){
+      const fileName = this.fileName
+      const img = document.getElementById('qrcode').getElementsByTagName('img')[0].src || ''
+      let aLink = document.createElement('a')
+      let blob = this.base64ToBlob(img)
+      let evt = document.createEvent('HTMLEvents')
+      evt.initEvent('click', true, true) // initEvent 不加后两个参数在FF下会报错  事件类型，是否冒泡，是否阻止浏览器的默认行为
+      aLink.download = fileName
+      aLink.href = URL.createObjectURL(blob)
+      aLink.click()
+    },
+    base64ToBlob(code) {
+      let parts = code.split(';base64,')
+      let contentType = parts[0].split(':')[1]
+      let raw = window.atob(parts[1])
+      let rawLength = raw.length
+      let uInt8Array = new Uint8Array(rawLength)
+      for (let i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i)
+      }
+      return new Blob([uInt8Array], { type: contentType })
+    },
   },
   created(){
     

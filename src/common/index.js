5aee151de45d86dedf54c2a7b76452abaa994a67
周爱桩 <840043348@qq.com>
2022-06-06 10:31:58 +0800

    动态导出函数

--- src/common/index.js
+++ src/common/index.js
@@ -1,54 +1,6 @@
-import {
-  shuffle, cached, toObject, sum, mean, adjust, array2Tree, once, makeMap, remove, repeat, sortByProp, noSame, getAreaFlat,
-  getField, chunk, splitAt, transformArr, splitWhen, intersect, difference, searchCover, groupBy, uniqueObj
-} from "./modules/array.js"
-import {
-  syncBgData, downloadFile, toFullScreen, exitFullscreen, random, randomColor, noCopy, getProps, safeGet, wait, deepCopy,
-  guID, getScrollTop, scrollPos, getCookie, clearCookies,queryAll, query, getLocalStorage, setLocalStorage,
-  getSessionStorage, setSessionStorage, getPosition, getViewHeight, getViewPos, showToast, removeCss, addCss, removeDom, addDom,
-  show, hide, showLoading, hideLoading, setTitle, goUrl, checkPwd, c2f, f2s, loadJs, loadCss
-} from "./modules/business.js"
-import {
-  isType,isArray,isObject, isEmptyObj, isReference, isNull, isUndefined, isFunction, isRegExp, isString, isNumber, isDate,
-  isError, isGt0, isGtEq0, inBrowser, inWeex, weexPlatform, UA, isIE, isIE9, isEdge, isAndroid, isIOS, isWeChat, isChrome, isPhantomJS,
-  isFF, isPhone, isIdentity, isEmail, getOS
-} from "./modules/check.js"
-import { compose, curry, debounce, throttling } from "./modules/func.js"
-import { formatMoney, round, range, largeNumAdd, toogle01 } from "./modules/number.js"
-import { extend, formatJSON, formatRes, checkJSON, JSON2url, invert, invertBy } from "./modules/object.js"
-import { regTest } from "./modules/reg.js"
-import { go, goBack, compareRoute } from "./modules/route.js"
-import { startWith, trim, sliceStr, addZero, length, copyLink, capitalize } from "./modules/string.js"
-import { map2Obj, obj2Map } from "./modules/structure.js"
-import { getDateStr, getDay, socketTime, dateFormater, afterNsecond, ms2Dhs, getDays, dayDiff, dayOfYear } from "./modules/timeDate.js"
-
-export {
-  // 数组
-  shuffle, cached, toObject, sum, mean, adjust, array2Tree, once, makeMap, remove, repeat, sortByProp, noSame, getAreaFlat,
-  getField, chunk, splitAt, transformArr, splitWhen, intersect, difference, searchCover, groupBy, uniqueObj,
-  // 业务代码
-  syncBgData, downloadFile, toFullScreen, exitFullscreen, random, randomColor, noCopy, getProps, safeGet, wait, deepCopy,
-  guID, debounce, throttling, getScrollTop, scrollPos, getCookie, clearCookies,queryAll, query, getLocalStorage, setLocalStorage,
-  getSessionStorage, setSessionStorage, getPosition, getViewHeight, getViewPos, showToast, removeCss, addCss, removeDom, addDom,
-  show, hide, showLoading, hideLoading, setTitle, goUrl, checkPwd, c2f, f2s, loadJs, loadCss,
-  // 校验
-  isType, isArray, isObject, isEmptyObj, isReference, isNull, isUndefined, isFunction, isRegExp, isString, isNumber, isDate,
-  isError, isGt0, isGtEq0, inBrowser, inWeex, weexPlatform, UA, isIE, isIE9, isEdge, isAndroid, isIOS, isWeChat, isChrome, isPhantomJS,
-  isFF, isPhone, isIdentity, isEmail, getOS,
-  // 高阶函数
-  compose, curry,
-  // 数字
-  formatMoney, round, range, largeNumAdd, toogle01,
-  // 对象
-  extend, formatJSON, formatRes, checkJSON, JSON2url, invert, invertBy,
-  // 正则
-  regTest,
-  // 路由
-  go, goBack, compareRoute,
-  // 字符串
-  startWith, trim, sliceStr, addZero, length, copyLink, capitalize,
-  // 数据结构
-  map2Obj, obj2Map,
-  // 时间日期处理
-  getDateStr, getDay, socketTime, dateFormater, afterNsecond, ms2Dhs, getDays, dayDiff, dayOfYear
-}
+const r = require.context('./modules', true, /\.js/)
+export const Z = r.keys().reduce((prev, item) => {
+  // console.log(item)
+  // console.log(r(item))
+  return {...prev, ...r(item)}
+}, {})

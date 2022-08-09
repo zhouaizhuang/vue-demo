c6e611fb11f08cb45020a9ba99f1ffbe92c290bb
周爱桩 <840043348@qq.com>
2022-08-09 11:49:54 +0800

    微信h5跳转小程序

---
+++ src/views/h5/goApplet/index.js
@@ -0,0 +1,45 @@
+import { loadJs } from "../../../common.js"
+import "../../../utils/px2rem.js"
+export default {
+  name: '',
+  components:{},
+  data(){
+    return {
+      
+    }
+  },
+  methods:{
+  },
+  async created(){
+    await loadJs("//res.wx.qq.com/open/js/jweixin-1.6.0.js") //加载微信js
+    // 调用接口获取配置-----> 
+    // 注意：
+    // 1、需要在微信公众号配置js安全域名，安全域名就是线上域名，只能在线上调试。比如https://health.gagctv.com/
+    // 2、后端需要微信公众号的appid、appsecrect，从而调用微信后台提供的接口生成accessToken
+    // 3、开启debug后，必须在微信浏览器中调试，才会有debug信息，出现config ok就正常了
+    // 4、注意：只支持在微信浏览器中通过这个标签进行跳转
+    const res = await this.$post('/robot/JsSdk') 
+    const {appId, timestamp, nonceStr, signature} = res
+    const url = window.location.href.split('#')[0]
+    const obj = {
+      debug: true,// 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: appId,         // 必填，公众号的唯一标识，填自己的！
+      timestamp: timestamp, // 必填，生成签名的时间戳，刚才接口拿到的数据
+      nonceStr: nonceStr,   // 必填，生成签名的随机串
+      signature: signature, // 必填，签名，见附录1
+      jsApiList: ['updateAppMessageShareData'],
+      url: encodeURIComponent(url),
+      openTagList: ["wx-open-launch-weapp"] // 跳转小程序时必填
+    }
+    wx.config(obj)
+    wx.ready(function (res) {
+      console.log(JSON.stringify(res))
+    })
+    wx.error(function (res) {
+      console.log(JSON.stringify(res))
+    })
+  },
+  mounted(){
+    
+  },
+}
---
+++ src/views/h5/goApplet/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="max-width: 420px;margin: auto;" class="bgf5 mh100vh">
+    <div class="rel" style="width:2.4rem;height:1.4rem;border-radius:80px;margin: 0 auto;">
+      <wx-open-launch-weapp
+        id="launch-btn"
+        username="gh_2d50fdf10999"
+        path="/pages/index/index.html"
+        style="position:absolute;top:0;left:0;z-index:100;width:100%;height:100%"
+      >
+        <script type="text/wxtag-template">
+          <div style="width:100%;height:150px;background-color:pink;opacity:0;">
+            去小程序中助力
+          </div>
+        </script>
+      </wx-open-launch-weapp>
+      <div class="abs l0 r0 t0">
+        <img src="https://upload.qianfanyun.com/olympic_btn_empty.png" class="w100 h100" />
+        <div class="abs l50 t50 txy-50 gf fs28r zx10 b nowrap">前往渐渐助手</div>
+      </div>
+    </div>
+  </div>
+</template>
+<script src='./index.js'></script>
+<style>
+</style>

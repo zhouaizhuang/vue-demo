7a04a7adc983b9c4d46102c79ca5c59a157354f8
周爱桩 <840043348@qq.com>
2022-08-29 14:33:36 +0800

    删除多余空格

--- src/common.js
+++ src/common.js
@@ -265,7 +265,7 @@ export const uniqueObj = function (arr, field, type) {
  *    {id: 1, name: '部门1', pid: 0},{id: 2, name: '部门2', pid: 1},{id: 3, name: '部门3', pid: 1},
  *    {id: 4, name: '部门4', pid: 3},{id: 5, name: '部门5', pid: 4}
  * ]
- * arrayToTree(arr) 
+ * arrayToTree(arr)
  * -----> 
  * [
  *   {

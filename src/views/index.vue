6cd54026c8105bbc5fdd43c36012790daf56b6e4
周爱桩 <840043348@qq.com>
2022-08-31 21:48:46 +0800

    调整文案

--- src/views/index.vue
+++ src/views/index.vue
@@ -67,8 +67,8 @@
         <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/drawImg">绘制图片</router-link></div>
         <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/studyCanvas">canvas学习</router-link></div>
         <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/barChart_pc">柱状图（带屏幕自适应）</router-link></div>
-        <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/canvasHover_pc">canvas（带Hover效果）</router-link></div>
-        <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/canvasClick_pc">canvas（点击效果）</router-link></div>
+        <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/canvasHover_pc">canvas（监听Hover）</router-link></div>
+        <div class="db w20 pb30 min190px"><router-link class="g42b983" to="/canvasClick_pc">canvas（监听点击）</router-link></div>
       </div>
     </div>
     <div class="fs50r b mt30 mb20 tc">通用组件</div>

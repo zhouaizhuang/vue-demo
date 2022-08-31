9127550e5a0d1244d9e285080e17d7dd598b7057
周爱桩 <840043348@qq.com>
2022-08-31 23:32:36 +0800

    增加解决方案

--- src/views/canvasAni/canvasClick_pc/index.vue
+++ src/views/canvasAni/canvasClick_pc/index.vue
@@ -1,3 +1,5 @@
+<!--更多检测方法：https://zhuanlan.zhihu.com/p/436494294-->
+<!--fibric.js库解决：https://juejin.cn/post/7026941253845516324-->
 <!--考虑扇形饼图的点击和hover：https://blog.csdn.net/qq_20423863/article/details/90701917-->
 <!--运用三角函数去计算，限制距离原型的距离，然后横纵坐标不超过多少-->
 <template>

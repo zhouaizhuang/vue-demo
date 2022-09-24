7dd0fae018d126d04b81b39d760bdb148d6d25a5
周爱桩 <840043348@qq.com>
2022-09-24 12:21:12 +0800

    调整main.js结构

--- src/main.js
+++ src/main.js
@@ -4,29 +4,23 @@ import router from './router'
 import store from './store'
 import "./common.css"
 import "./utils/px2rem.js"
-Vue.config.productionTip = false
-// 使用自定义指令
 import Directives from "./directives/index.js"
-Vue.use(Directives)
-// 引入iview
 import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
-Vue.use(ViewUI)
-// 批量注册全局组件
 import globalCommponent from "./globalComponents/index.js"
-Vue.use(globalCommponent)
 import Print from "./utils/print.js"
-Vue.use(Print)
-// axios全局挂载vue实例
 import network from "./utils/network.js"
-Vue.use(network)
-// 全局挂载自定义函数库
 import * as Z from './common.js'
-Vue.prototype.$Z = Z
-Z.reWriteLog() // 关闭生产环境的console.log
-// 极验验证
 import SlideVerify from 'vue-monoplasty-slide-verify'
-Vue.use(SlideVerify)
+Vue.config.productionTip = false
+Vue.use(Directives) // 引入自定义指令
+Vue.use(ViewUI) // 引入iview
+Vue.use(globalCommponent) // 批量注册全局组件
+Vue.use(Print) // 引入打印功能
+Vue.use(network) // axios全局挂载vue实例
+Vue.prototype.$Z = Z // 全局挂载自定义函数库
+Z.reWriteLog() // 关闭生产环境的console.log
+Vue.use(SlideVerify) // 极验验证
 Vue.config.ignoredElements = ['wx-open-launch-weapp']
 
 new Vue({

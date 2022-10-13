fa95f2ed46bf2532a82a6bcb43484b012005d64c
周爱桩 <840043348@qq.com>
2022-10-14 00:25:22 +0800

    最小高度设置

--- src/views/tools/desc_pc/index.vue
+++ src/views/tools/desc_pc/index.vue
@@ -49,8 +49,8 @@
       <div class="f1 f xc">转换结果</div>
     </div>
     <div class="f pl15 pr15">
-      <div class="f1 ovh">
-        <Input v-model="oldVal" type="textarea" @on-change="covertVal" style="height:500px;" :autosize="{minRows: 25,maxRows: 100}" placeholder="请输入需要转换的值"></Input>
+      <div class="f1 ">
+        <Input v-model="oldVal" type="textarea" @on-change="covertVal" style="min-height:500px;" :autosize="{minRows: 25,maxRows: 100}" placeholder="请输入需要转换的值"></Input>
       </div>
       <div v-if="autoCapacity" :class="['fs18 f ac xc ml40 mr40', stateText.includes('成功') ? 'g1890ff' : stateText.includes('失败') ? 'gf10000' : 'g1890ff']" style="width:0;height:500px;">{{stateText}}</div>
       <div v-show="!autoCapacity" class="pt100" style="height:400px;width:80px;">

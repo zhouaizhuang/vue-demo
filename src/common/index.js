61add90ee1169335b47afc14323c7d46fca3c745
周爱桩 <840043348@qq.com>
2022-10-13 21:01:21 +0800

    删除多余的js文件

--- src/common/index.js
+++
@@ -1,6 +0,0 @@
-const r = require.context('./modules', true, /\.js/)
-export const Z = r.keys().reduce((prev, item) => {
-  // console.log(item)
-  // console.log(r(item))
-  return {...prev, ...r(item)}
-}, {})
--- src/common/modules/reg.js
+++
@@ -1,7 +0,0 @@
-/**正则校验返回true || false
- * @param {String} val 需要正则校验的值
- * @param {reg} reg 正则表达式
- * @returns {Boolean}
- * @举例 regTest(12, /[0-9]/) -----> true
- */
- export const regTest = (val, reg) => new RegExp(reg).test(val)

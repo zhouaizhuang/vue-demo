1d5a5237cb6c028cdd56c401da89d7416d125f41
周爱桩 <840043348@qq.com>
2022-10-26 14:30:33 +0800

    修复指令在输入中文之后，v-model失效问题

--- src/directives/modules/check.js
+++ src/directives/modules/check.js
@@ -1,25 +1,43 @@
 /**
+ * 处理中文输入的情况
+ * @param {*} ref 处理后的dom句柄
+ * @param {*} el 当前绑定的dom
+ * @param {*} vnode dom
+ * @param {*} fn 处理函数
+ */
+ const resolveChar = function (ref, vnode, fn){
+  ref.addEventListener('compositionstart', () => {
+    vnode.inputLocking = true
+  })
+  ref.addEventListener('compositionend', () => {
+    vnode.inputLocking = false
+    ref.dispatchEvent(new Event('input'))
+  })
+  ref.addEventListener('input', fn, true)
+}
+/**
  * 限制整数
  * @param {Function} 
  * 直接使用： <Input v-int></Input>
  * 传入true，允许输入负值： <Input v-int="true"></Input>
  */
-export const int = {
-  inserted(el, {value}){
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+ export const int = {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value.replace(eval(`/[^0-9${value ? '-' : ''}]/g`), '')
       const symbol = tmp.at(0) == '-' ? '-' : ''
       if(symbol === '-') { tmp = tmp.slice(1) }
       tmp = tmp.replace(/-/g, '')
       tmp = tmp == '' ? '' : (tmp.replace(/^0+/g, '') || '0')
-      inputRef.value = symbol + tmp
+      tmp = symbol + tmp
+      inputRef.value = tmp
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    resolveChar(inputRef, vnode, fn)
   },
 }
 /**
@@ -29,9 +47,12 @@ export const int = {
  * 传入true使用，允许输入负值： <Input v-float="true"></Input>
  */
 export const float = {
-  inserted(el, {value}){
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value
       tmp = tmp.replace(eval(`/[^0-9.${value ? '-' : ''}]/g`), '')
       const symbol = tmp.at(0) == '-' ? '-' : ''
@@ -48,16 +69,15 @@ export const float = {
       }, {tmp:'', dotNum: 0})['tmp']
       if(tmp.includes('.') && /-?([0-9]*).([0-9]*)/.test(tmp)) {
         const [, left, right] = tmp.match(/-?([0-9]*).([0-9]*)/)
-        inputRef.value = symbol + (left.replace(/^[0]+/g, '') || '0') + '.' + right
+        tmp = symbol + (left.replace(/^[0]+/g, '') || '0') + '.' + right
       } else {
-        inputRef.value = symbol + (tmp == '' ? '' : (tmp.replace(/^[0]+/, '') || '0'))
+        tmp = symbol + (tmp == '' ? '' : (tmp.replace(/^[0]+/, '') || '0'))
       }
+      inputRef.value = tmp
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    if(!inputRef) { inputRef = el }
+    resolveChar(inputRef, vnode, fn)
   }
 }
 /**
@@ -66,18 +86,18 @@ export const float = {
  * 直接使用： <Input v-code></Input>
  */
 export const code = {
-  inserted(el, { value }){
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value
       tmp = tmp.replace(/[^0-9A-Za-z!@#$%^&*()_+{}?><|/.,`\uff08\uff09\u3008\u3009\u300a\u300b\u300c\u300d\u300e\u300f\ufe43\ufe44\u3014\u3015\u2026\u2014\uff5e\ufe4f\uffe5\u3001\u3010\u3011\uff0c\u3002\uff1f\uff01\uff1a\uff1b\u201c\u201d\u2018\u2019]/g, '')
       inputRef.value = tmp
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    resolveChar(inputRef, vnode, fn)
   }
 }
 /**
@@ -86,17 +106,18 @@ export const code = {
  * 直接使用： <Input v-limit="5"></Input>
  */
  export const limit = {
-  inserted(el, { value }){
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value
-      inputRef.value = tmp.slice(0, Number(value))
+      tmp = tmp.slice(0, Number(value))
+      inputRef.value = tmp
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    resolveChar(inputRef, vnode, fn)
   }
 }
 /**
@@ -105,22 +126,23 @@ export const code = {
  * 直接使用： <Input v-decimalLimit="2"></Input>
  */
  export const decimalLimit = {
-  inserted(el, { value }){
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value
       if(tmp.includes('.') && /-?([0-9]*).([0-9]*)/.test(tmp)) {
         const [, left, right] = tmp.match(/-?([0-9]*).([0-9]*)/)
         if(right.length > 2) {
-          inputRef.value = left ? (tmp.at(0) == '-' ? '-' : '') + left + '.' + right.slice(0, Number(value)) : ''
+          tmp = left ? (tmp.at(0) == '-' ? '-' : '') + left + '.' + right.slice(0, Number(value)) : ''
         }
+        inputRef.value = tmp
       }
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    resolveChar(inputRef, vnode, fn)
   }
 }
 /**
@@ -129,17 +151,19 @@ export const code = {
  * 直接使用： <Input v-min="0"></Input>
  */
 export const min = {
-  inserted(el, { value }) {
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value
-      inputRef.value = tmp === '' ? '' : Math.max(Number(tmp) || 0, Number(value))
+      tmp = tmp === '' ? '' : Math.max(Number(tmp) || 0, Number(value))
+      tmp = String(tmp)
+      inputRef.value = tmp
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    resolveChar(inputRef, vnode, fn)
   }
 }
 /**
@@ -148,16 +172,18 @@ export const min = {
  * 直接使用： <Input v-max="100"></Input>
  */
  export const max = {
-  inserted(el, { value }) {
-    const inputRef = el.querySelector('input')
-    const fn = () => window.requestAnimationFrame(() => {
+  inserted(el, {value}, vnode) {
+    const inputRef = el.querySelector('input') || el
+    const fn = e => window.requestAnimationFrame(() => {
+      e.preventDefault()
+      if (vnode.inputLocking) { return }
+      let originVal = inputRef.value
       let tmp = inputRef.value
-      inputRef.value = tmp === '' ? '' : Math.min(Number(tmp) || 0, Number(value))
+      tmp = tmp === '' ? '' : Math.min(Number(tmp) || 0, Number(value))
+      tmp = String(tmp)
+      inputRef.value = tmp
+      if(originVal != tmp) { inputRef.dispatchEvent(new Event('input')) }
     })
-    if(inputRef) {
-      inputRef.addEventListener('input', fn)
-    } else {
-      el.addEventListener('input', fn)
-    }
+    resolveChar(inputRef, vnode, fn)
   }
 }

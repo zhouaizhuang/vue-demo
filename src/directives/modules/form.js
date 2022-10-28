50d33097f88074057a77cc1a3299a86d9f5281f5
周爱桩 <840043348@qq.com>
2022-10-28 11:11:43 +0800

    将v-focus移动到form中

--- src/directives/modules/form.js
+++ src/directives/modules/form.js
@@ -210,3 +210,15 @@ export const min = {
     fatherDom.appendChild(op10Mask)
   },
 }
+/**
+ * input自动聚焦指令
+ * @param {Function} 
+ * 直接使用： <Input v-focus></Input>
+ */
+export const focus = {
+  inserted: function (el) {
+    // 聚焦元素
+    const inputRef = el.querySelector('input')
+    inputRef ? inputRef.focus() : el.focus()
+  }
+}

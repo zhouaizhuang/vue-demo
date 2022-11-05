444bb0d0ec316592e063ff71918d4ea36105a607
周爱桩 <840043348@qq.com>
2022-11-05 09:53:42 +0800

    调整icon

--- src/views/commCmpt/icon/index.vue
+++ src/views/commCmpt/icon/index.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="fixed trbl0" style="background: rgba(0,0,0,.8);">
-    <!-- 钩 -->
-    <div class="abs rds50 l10 t10 tr-45" style="width:80px;height:80px;border:4px solid #fff;background-color: #7abd3b;" >
-      <div class="w50 h25 rds5 abs l50 t50 " style="transform: translate(-40%,-60%);border: 7px solid #fff;border-top:none;border-right:none;background: transparent;"></div>
-    </div>
-    <!-- 叉 -->
-    <div class="abs rds50 l40 t10 tr-45" style="width:80px;height:80px;border:4px solid #fff;background-color: #fd2f2d;">
-      <div class="w85 h8 abs l50 t50 txy-50 ty-50 rds5 bgf" style="transform-origin:center center;"></div>
-      <div class="w8 h85 abs l50 t50 txy-50 bgf rds5" style="transform-origin:center center;"></div>
-    </div>
-    <!-- 叉 -->
-    <div class="abs rds50 r30 t10 tr-45" style="width:80px;height:80px;border: 4px solid #fff;">
-      <div class="w85 h7 abs l50 t50 txy-50 rds5 bgf" style="transform-origin:center center;"></div>
-      <div class="w7 h85 abs l50 t50 txy-50 rds5 bgf" style="transform-origin:center center;"></div>
+    <div class="f rw pt30 pl60">
+      <!-- 钩 -->
+      <div class="mb30 f ac xc" style="min-width:140px;">
+        <div class="rds50 t10 tr-45" style="width:80px;height:80px;border:4px solid #fff;background-color: #7abd3b;" >
+          <div class="w50 h25 rds5 abs l50 t50 " style="transform: translate(-40%,-60%);border: 7px solid #fff;border-top:none;border-right:none;background: transparent;"></div>
+        </div>
+      </div>
+      <!-- 叉 -->
+      <div class="mb30 f ac xc" style="min-width:140px;">
+        <div class="rds50 tr-45" style="width:80px;height:80px;border:4px solid #fff;background-color: #fd2f2d;">
+          <div class="w85 h8 abs l50 t50 txy-50 ty-50 rds5 bgf" style="transform-origin:center center;"></div>
+          <div class="w8 h85 abs l50 t50 txy-50 bgf rds5" style="transform-origin:center center;"></div>
+        </div>
+      </div>
+      <!-- 叉 -->
+      <div class="mb30 f ac xc" style="min-width:140px;">
+        <div class="rds50 tr-45" style="width:80px;height:80px;border: 4px solid #fff;">
+          <div class="w85 h7 abs l50 t50 txy-50 rds5 bgf" style="transform-origin:center center;"></div>
+          <div class="w7 h85 abs l50 t50 txy-50 rds5 bgf" style="transform-origin:center center;"></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>

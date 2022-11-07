550dfaff0ffe88490a1e5b4b183e8ad8a803a000
周爱桩 <840043348@qq.com>
2022-11-07 17:22:47 +0800

    增加注释

--- src/views/h5/barCode/index.js
+++ src/views/h5/barCode/index.js
@@ -1,4 +1,9 @@
-// 对条形码图片质量要求很高，长条形码需要将图片放大扫描，否则误码率高。容易识别不出来
+/**
+ * 注意事项：
+ * 1、条形码如果宽度很小，会导致条码黑白色间距太小，进而导致quagga库无法识别
+ * 2、因此，要注意条形码必须搞大点。这样识别率会非常高。   像这样的： 850F0DA1ADFB091C9生成的code码，大图是很容易识别的。
+ * 在线生成code128编码的网址： https://www.fontke.com/tool/code128/
+ */
 import * as func from "./func.js"
 export default {
   name: '',
@@ -7,7 +12,6 @@ export default {
     return {
       isShowBarCode: false, // 是否显示一维码扫描弹窗
       serviceCode: '',
-      deviceCode: '',
     }
   },
   methods:{

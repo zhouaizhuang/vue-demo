73cc910f960abba8bd5f525e5b0b2620283c3478
周爱桩 <840043348@qq.com>
2022-11-07 17:25:29 +0800

    增加注释一维码和二维码的解析

--- src/views/h5/scanfCode/index.js
+++ src/views/h5/scanfCode/index.js
@@ -1,6 +1,10 @@
 // 不依赖微信环境的摄像头扫描识别二维码（可用）： https://blog.csdn.net/muguli2008/article/details/114134547
 // https://blog.csdn.net/m0_47763320/article/details/126726681
 // https://github.com/zxing/zxing
+/**
+ * 1、注意：这个库能同时支持识别一维码和二维码
+ * 2、如果是一维码，必须要保证图片长度足够，否则的话。黑白色间距过小。会导致无法识别
+ */
 import { BrowserMultiFormatReader } from '@zxing/library'
 import * as func from "./func.js"
 export default {

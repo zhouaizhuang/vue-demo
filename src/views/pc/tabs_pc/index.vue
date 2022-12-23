948418c1e164409b5a6afeeb8e883b2e41925e97
周爱桩 <840043348@qq.com>
2022-12-23 10:35:27 +0800

    调整盒子高度

--- src/views/pc/tabs_pc/index.vue
+++ src/views/pc/tabs_pc/index.vue
@@ -7,7 +7,7 @@
         <div @click="tabIndex=1" class="mr16 poi pt10 pb10" :style="{color: tabIndex == 1 ? '#2d8cf0' : '', width: '88px'}">标签2</div>
         <div class="abs b0 trans3" :style="{backgroundColor:'#2d8cf0',width:'86px',height:'2px',transform: `translateX(${104 * tabIndex}px)`}"></div>
       </div>
-      <div class="ovh rel fs14" style="height:630px;"> <!--高度必须限制（可以是固定值或者给定上下限-----width:clamp(200px, 50vh, 600px)）-->
+      <div class="ovh rel fs14" style="height:450px;"> <!--高度必须限制（可以是固定值或者给定上下限-----width:clamp(200px, 50vh, 600px)）-->
         <div :class="['trans3 abs trbl0 bgf pt10 pr10 pl0', tabIndex == 0 ? 'tx0' : tabIndex < 0 ? 'tx100' : 'tx-100']">
           我是标签1的内容
         </div>

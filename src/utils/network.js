b1a9e5bc54284c9ef1e44327e8bee0ef45943782
周爱桩 <840043348@qq.com>
2023-01-12 10:20:35 +0800

    网络请求更完善的封装

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,93 +1,148 @@
 import axios from "axios"
-import { JSON2url, formatJSON, showToast, getLocalStorage } from "../common.js"
-// console.log(process.env.VUE_APP_BASE_URL)
-const isDebug = process.env.VUE_APP_ENV == 'development'
+import CryptoJS from 'crypto-js'
+import { formatJSON, getSessionStorage, removeSessionStorage, downloadFile, safeGet, dateFormater } from "@/common.js"
+import router from "../router/index.js"
+import Qs from 'qs'
+// import { Message } from 'view-design'
+/**
+ * 加密解密函数
+ * encrypt： 加密函数
+ * decrypt： 解密函数
+ */
+const utf8Key = CryptoJS.enc.Utf8.parse(process.env.VUE_APP_KEY) // 秘钥
+const options = { iv: CryptoJS.enc.Utf8.parse(process.env.VUE_APP_IV), padding: CryptoJS.pad.Pkcs7, mode: CryptoJS.mode.CBC } // 生成配置项
+export const encrypt = e => CryptoJS.TripleDES.encrypt(e, utf8Key, options).toString() // 加密
+export const decrypt = e => CryptoJS.enc.Utf8.stringify(CryptoJS.TripleDES.decrypt(e.replace(/\s/g,''), utf8Key, options)) // 解密
+/**
+ * 页面调试debug开关
+ * isDebug： true ----> 代表当前是开发环境不需要加密
+ * isDebug： false ----> 代表当前是生产环境需要加密
+ */
+export const isDebug = process.env.VUE_APP_ENV == 'development' // 是否开启debug
+export const requestUrl = isDebug ? process.env.VUE_APP_BASE_URL : `${window.location.protocol}//${window.location.host}`
 /************配置axios****** */
 let service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_URL,
+  baseURL: requestUrl,
   timeout: 10e3
 })
 /*
-* 设置请求拦截器
-* token一般存在vuex/redux/本地存储中
-*/
+ * 设置请求拦截器
+ * token一般存在vuex/redux/本地存储中
+ */
 service.interceptors.request.use(
   config => {
-    // const token = getLocalStorage('token')
-    // if(token) {
-    //   // config.headers.Authorization = token
-    //   config.headers['token'] = token
-    // }
+    const token = getSessionStorage('token') // 测试token:  "100.41b857bed9564d2792a5a995dbea44d7"
+    if (token) {
+      config.headers['token'] = token
+    }
+    if (isDebug) {
+      config.headers["Content-Type"] = "application/x-www-form-urlencoded"
+    } else {
+      config.headers.Accept = "application/json"
+    }
     return config
   },
-  err => {
-    return Promise.reject(err)
-  }
+  err => Promise.reject(err)
 )
 /*
-* 响应拦截器
-*/
-// service.interceptors.response.use(
-//   res => { return res.data },
-//   err => {
-//     const { response } = err
-//     if (response) {
-//       switch (response.status) {
-//         case 401: break // 权限问题，未登录
-//         case 403: window.localStorage.removeItem('token'); break// token过期
-//         case 404: break // 找不到页面
-//       }
-//     } else {
-//       // 服务器结果都没返回（可能服务器崩了，或者断网了
-//       if (!window.navigator.onLine) {
-//         // 跳转到断网页面
-//         console.log('你的网络连接有问题')
-//         return
-//       }
-//       return Promise.reject(err)
-//     }
-//   }
-// )
-
-// 响应拦截demo2
-// service.interceptors.response.use(
-//   res => {
-//     const {code, data, msg} = res
-//     if(code == '401') {
-//       location.href = '/login'
-//     }
-//     return Promise.resolve(res)
-//   },
-//   err => Promise.reject(err)
-// )
-// 封装一个request请求
-export const request = function (options) {
-  return service.request({
-    method: "GET",
-    url: '',
-    data: {},
-    ...options
-  }).then(res => {
-    const {errcode, errmsg} = res
-    if(errcode == 200) {
-      return res
-    } else if(errcode == 10200 || errcode == 10210) { // 未登录和token过期
-      removeSessionStorage('token')
-      removeSessionStorage('roleId')
-      Message.info('请登录')
-      router.replace({ path: "/login" }).catch(() => {})
-      return false
-    } else { // 其他异常
-      Message.error(JSON.stringify(errmsg) || `${options.url}报错，errmsg为空`);
-      return false
+ * 响应拦截器
+ */
+service.interceptors.response.use(
+  res => {
+    // 加密模式 && 不是文件流请求
+    if ((!isDebug) && !(res.data instanceof Blob)) {
+      res.data = res.data && res.data.errcode ? res.data : JSON.parse(decrypt(res.data))
     }
-  })
-}
+    return res.data.errcode ? res.data : res
+  },
+  err => {
+    const { response } = err
+    if (response) {
+      switch (response.status) {
+        case 401: break // 权限问题，未登录
+        case 403: removeSessionStorage('token'); break // token过期
+        case 404: break // 找不到页面
+      }
+    } else {
+      if (!window.navigator.onLine) { return Promise.reject('你的网络连接有问题') } // 服务器结果都没返回（可能服务器崩了，或者断网了
+      return Promise.reject(err)
+    }
+  }
+)
 // 处理入参
 const resolveParams = params => {
   params = formatJSON(params || {})
-  let postParam = isDebug ? { ...params, ...commonParam, debug: 1 } : { data: DES3.encrypt(JSON.stringify({...params, ...commonParam})) }
-  return JSON2url('', postParam)
+  const commonParam = { cityLcode: '320400', deviceId: 'PC', deviceType: 'PC', clientTime: new Date().getTime() }
+  let postParam = isDebug ? { ...params, ...commonParam, debug: 1 } : { data: encrypt(JSON.stringify({ ...params, ...commonParam })) }
+  return Qs.stringify(postParam)
+}
+// 处理异常
+export const processError = function (res, url, type, resolve, reject) {
+  // res = safeGet(() => res.errcode, false) ? res : JSON.parse(decrypt(res))
+  const { errcode, data, errmsg } = res
+  if (errcode == 10200 || errcode == 10210) { // 未登录和token过期
+    removeSessionStorage('token')
+    removeSessionStorage('roleId')
+    router.replace({ path: "/login" }).catch(() => {})
+    iview.Message.info('请登录')
+    reject('请登录')
+  } else if (type == 0) {
+    resolve(res)
+  } else if (errcode == 200) {
+    resolve(data)
+  } else { // 其他异常
+    iview.Message.error(JSON.stringify(errmsg) || `${url}报错，errmsg为空`)
+    reject(JSON.stringify(errmsg) || `${url}报错，errmsg为空`)
+  }
+}
+/**************************************************************************************************** */
+/******************************************分******************************************************** */
+/******************************************割******************************************************** */
+/******************************************线******************************************************** */
+/**************************************************************************************************** */
+// 上传图片
+export const uploadImg = function (url, formDatas, type = 1) {
+  if(isDebug) { formDatas.append('debug',1) }
+  return new Promise((resolve, reject) => {
+    return service.request({
+      method: "POST",
+      headers: { 'Content-Type': 'multipart/form-data' },
+      url,
+      data: formDatas,
+      params: formDatas,
+      transformRequest: [function () { return formDatas }],
+    }).then(res => processError(res, url, type, resolve, reject))
+  })
+}
+//导出文件
+export const exportFile = function (url, params) {
+  return service.request({ method: "POST", url, data: resolveParams(params), responseType: 'blob' }).then(res => {
+    if (!(res.headers['content-disposition'] && res.data)) { return Message.error('导出失败') }
+    let fileName = safeGet(() => decodeURI(res.headers['content-disposition'].split('=')[1]), dateFormater('YYYYMMDDhhmmss') + '.txt')
+    downloadFile(fileName, res.data)
+    return iview.Message.success('导出成功')
+  })
+}
+// 封装一个request请求
+export const request = function (options) {
+  return new Promise((resolve, reject) => {
+    return service.request({ method: "GET", url: '', data: {}, ...options}).then(res => {
+      const { errcode, errmsg } = res
+      if (errcode == 200) {
+        resolve(res)
+      } else if (errcode == 10200 || errcode == 10210) { // 未登录和token过期
+        removeSessionStorage('token')
+        removeSessionStorage('roleId')
+        iview.Message.info('请登录')
+        router.replace({ path: "/login"}).catch(() => {})
+        reject('请登录')
+      } else { // 其他异常
+        const msg = JSON.stringify(errmsg) || `${options.url}报错，errmsg为空`
+        iview.Message.error(msg)
+        reject(msg)
+      }
+    })
+  })
 }
 /**
  * 封装一个post请求
@@ -97,55 +152,28 @@ const resolveParams = params => {
  * @returns 
  */
 export const post = function (url, params, type = 1) {
-  return new Promise((resolve, reject) => {
-    return service.post(url, resolveParams(params)).then(res => processError(res, url, type, resolve, reject))
-  })
+  return new Promise((resolve, reject) => service.post(url, resolveParams(params)).then(res => processError(res, url, type, resolve, reject)))
 }
-// 处理异常
-export const processError = function (res, url, type, resolve, reject) {
-  res = safeGet(() => res.errcode, false) ? res : JSON.parse(DES3.decrypt(res))
-  const  {errcode, data, errmsg} = res
-  if(errcode == 10200 || errcode == 10210) {  // 未登录和token过期
-    removeSessionStorage('token')
-    removeSessionStorage('roleId')
-    router.replace({ path: "/login" }).catch(() => {})
-    Message.info('请登录')
-    reject('请登录')
-  } else if(type == 0) {
-    resolve(res)
-  } else if(errcode == 200) {
-    resolve(data)
-  } else { // 其他异常
-    Message.error(JSON.stringify(errmsg) || `${url}报错，errmsg为空`);
-    reject(JSON.stringify(errmsg) || `${url}报错，errmsg为空`)
-  }
-}
-// https://juejin.cn/post/7033395086696136711#heading-12
-/**************************************************************************************************** */
-/******************************************分******************************************************** */
-/******************************************割******************************************************** */
-/******************************************线******************************************************** */
-/**************************************************************************************************** */
 /**
  * 防止重复请求，只请求第一次，在第一次结果返回之前。后续相同的url请求被屏蔽
  * @param {*} url 请求的url地址
  * @param {*} params 请求参数
  * @param {*} type 返回数据类型  1代表只返回data    0代表不做任何处理直接返回格式：{code:xx, data:xx, msg:xx}
  * @returns 
- * @举例 const res = this.$startPost('/apprepair/editRepair', {id:1, name:'zz'})
+ * @举例 const res = this.$endPost('/apprepair/editRepair', {id:1, name:'zz'})
  */
 export const startPost = (function () {
   const reqRecord = new Map() // 记录已发起但未返回的请求： url<--->reject方法
-  return function (url, params, type = 1){
+  return function (url, params, type = 1) {
     return new Promise((resolve, reject) => {
-      if(reqRecord.get(url)) { return Promise.reject(`取消当前请求${url}`) }
+      if (reqRecord.get(url)) { return Promise.reject(`取消当前请求${url}`) }
       reqRecord.set(url, url)
       return service.post(url, resolveParams(params)).then(res => processError(res, url, type, resolve, reject)).finally(() => reqRecord.delete(url))
     })
   }
 })()
 /**
- * 可以重复请求，统统的url请求，只会渲染最后一次请求返回的结果
+ * 可以重复请求，连续重复的url请求，只会渲染最后一次请求返回的结果
  * @param {*} url 请求的url地址
  * @param {*} params 请求参数
  * @param {*} type 返回数据类型  1代表只返回data    0代表不做任何处理直接返回格式：{code:xx, data:xx, msg:xx}
@@ -154,12 +182,10 @@ export const startPost = (function () {
  */
 export const endPost = (function () {
   const reqRecord = new Map() // 记录已发起但未返回的请求： url<--->reject方法
-  return function(url, params, type = 1){
+  return function (url, params, type = 1) {
     const req = () => service.post(url, resolveParams(params))
     return new Promise((resolve, reject) => {
-      if(reqRecord.get(url)) {
-        reqRecord.get(url)(`放弃上次请求的渲染${url}`) // 通过保存的reject地址，对接口进行reject
-      }
+      if (reqRecord.get(url)) { reqRecord.get(url)(`放弃上次请求的渲染${url}`) } // 放弃请求
       const promiseA = new Promise((_, rej) => reqRecord.set(url, rej))
       return Promise.race([req(), promiseA]).then(res => processError(res, url, type, resolve, reject)).finally(() => reqRecord.delete(url))
     })
@@ -169,9 +195,11 @@ export const endPost = (function () {
 // 注册到全局vue
 export default {
   install(Vue) {
+    Vue.prototype.$exportFile = exportFile
+    Vue.prototype.$uploadImg = uploadImg
     Vue.prototype.$request = request
     Vue.prototype.$post = post
     Vue.prototype.$startPost = startPost
     Vue.prototype.$endPost = endPost
-  },
+  }
 }

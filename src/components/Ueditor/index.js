c93bcf0f52bc95fb3088ae8fa702a9d6c25e702e
周爱桩 <840043348@qq.com>
2023-01-12 10:20:12 +0800

    富文本优化

--- src/components/Ueditor/index.js
+++ src/components/Ueditor/index.js
@@ -1,4 +1,5 @@
 import VueUeditorWrap from 'vue-ueditor-wrap'
+import { requestUrl } from "@/utils/network.js"
 export default {
   name: 'Editor',
   components: {
@@ -29,6 +30,7 @@ export default {
         * java参考资料： https://www.freesion.com/article/8122633306/
         * 注意：当前文件夹下的config.json是后端的参考json配置模板
         */
+        // serverUrl: `${requestUrl}/api/config`, // 后端把代码部署到对应服务器，前端动态域名
         serverUrl: 'http://hd.qianfanyun.com/php/controller.php?action=config', // 'http://192.168.10.41:8389/api/config'
         UEDITOR_HOME_URL: '/UEditor/', // 注意：此处是UEditor文件夹放在public文件夹中
         maximumWords:'100000000'

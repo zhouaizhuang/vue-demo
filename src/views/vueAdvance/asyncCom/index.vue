cf2b0d3ed55ef6a1d3786702b4e60b9ec8523ca5
周爱桩 <840043348@qq.com>
2023-01-23 16:10:43 +0800

    样式微调

--- src/views/vueAdvance/asyncCom/index.vue
+++ src/views/vueAdvance/asyncCom/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mh100vh h100vh">
     <div class="tc f xb pl50 pr50 h100 pb50 ae">
-      <button @click="message('success')">成功提示</button>
-      <button @click="message('info')">消息提示</button>
+      <Button type="primary" @click="message('success')">成功提示</Button>
+      <Button type="info" @click="message('info')">消息提示</Button>
     </div>
   </div>
 </template>
--- src/views/vueAdvance/myVmodel/index.vue
+++ src/views/vueAdvance/myVmodel/index.vue
@@ -1,6 +1,6 @@
 <!--自定义组件的v-model使用方式-->
 <template>
-  <div class="auto fs20r h100vh fs50r">
+  <div class="auto fs14 h100vh fs25">
     <advVmodel v-model="text" />
   </div>
 </template>

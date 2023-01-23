386e100c8d129f384e893440ed4217b2eaacca1e
周爱桩 <840043348@qq.com>
2023-01-23 15:12:55 +0800

    組件封裝

--- src/views/commCmpt/zInput/index.vue
+++ src/views/commCmpt/zInput/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <div style="width:7.5rem" class="auto h100vh bgf">
-    <div class="pl30r pr30r pt100r">
+  <div style="" class="auto h100vh bgf">
+    <div class="pl15 pr15 pt50">
       <!--1、自带边框-->
-      <div class="f ac mb40r">
-        <div class="f fs28r lt5r require" style="width:1.2rem;">姓名:</div>
-        <div class="f1 rel fs28r rds10r bdc" style="height: 0.7rem;">
-          <input type="text" @input="inputName" v-model="nameObj.value" id='myName' :class="['h90 bd0 abs borderBox w98 pl5r db b fs28r', nameObj.state ? 'g5e667a' : 'gred']" style="top:0.03rem;left:0.03rem;" />
-          <label v-if="!nameObj.value " class="g5C6579 abs t50 ty-50 fs28r ga0aac0" for="myName" style="left: 0.15rem;">请输入姓名</label>
+      <div class="f ac mb20">
+        <div class="f fs14 lt5 require" style="width:60px;">姓名:</div>
+        <div class="f1 rel fs12 rds5 bdc" style="height: 35px;">
+          <input type="text" @input="inputName" v-model="nameObj.value" id='myName' :class="['h88 bd0 abs borderBox w98 pl2 db b fs14', nameObj.state ? 'g5e667a' : 'gred']" style="top:2px;left:3px;" />
+          <label v-if="!nameObj.value " class="g5C6579 abs t50 ty-50 fs14 ga0aac0" for="myName" style="left: 7px;">请输入姓名</label>
         </div>
       </div>
       <!--2、只有底线的情况-->
-      <div class="f ac mb40r">
-        <div class="f fs28r lt5r require" style="width:1.2rem;">昵称:</div>
-        <div class="f1 rel fs28r bde5" style="height: 0.7rem;">
-          <input type="text" @input="inputNickName" v-model="nickNameObj.value" id='nickName' :class="['h90 bd0 abs borderBox w98 pl5r db b fs28r', nickNameObj.state ? 'g5e667a' : 'gred']" style="top:0.03rem;left:0.03rem;" />
-          <label v-if="!nickNameObj.value " class="g5C6579 abs t50 ty-50 fs28r ga0aac0" for="nickName" style="left: 0.15rem;">请输入昵称</label>
+      <div class="f ac mb20">
+        <div class="f fs14 lt5 require" style="width:60px;">昵称:</div>
+        <div class="f1 rel fs14 bdbe5" style="height:30px;">
+          <input type="text" @input="inputNickName" v-model="nickNameObj.value" id='nickName' :class="['h90 bd0 abs borderBox w98 pl5 db b fs14', nickNameObj.state ? 'g5e667a' : 'gred']" style="top:3px;left:3px;" />
+          <label v-if="!nickNameObj.value " class="g5C6579 abs t50 ty-50 fs14 ga0aac0" for="nickName" style="left: 15px;">请输入昵称</label>
         </div>
       </div>
     </div>
@@ -48,4 +48,7 @@ export default {
 }
 </script>
 <style scoped>
+.bdbe5{
+  border-bottom: 1px solid #e5e5e5;
+}
 </style>

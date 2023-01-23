4abfa41167a1b169da3271c3c10229acd27d2f4e
周爱桩 <840043348@qq.com>
2023-01-23 20:52:36 +0800

    增加注释

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -1,4 +1,17 @@
 import * as func from "./func.js"
+/**
+ * 使用方法
+ *  <Tabs
+      :titleArr="['标签1','标签2','标签3', '标签4']" // 标签名
+      :tabIndex="tabIndex" // 当前所在条目
+      @setTabIndex="e=>tabIndex=e"  // 固定写法
+    >
+      <template #tab0>我是标签1的内容</template> <!--传值的时候可以使用es6的解构, #tab0="{tab0}"-->
+      <template #tab1>我是标签2的内容</template>
+      <template #tab2>我是标签3的内容</template>
+      <template #tab3>我是标签4的内容</template>
+    </Tabs>
+ */
 export default {
   name: '',
   props: {

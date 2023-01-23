0d5347f9b106e5d0034888936d3b63ae0f9254bb
周爱桩 <840043348@qq.com>
2023-01-23 16:17:01 +0800

    样式调整

--- src/views/vueAdvance/params2/index.vue
+++ src/views/vueAdvance/params2/index.vue
@@ -1,9 +1,9 @@
 
 <template>
-  <div class="auto fs20r h100vh fs50r">
-    <div class="fs30r pt30r pb30r pl30r">这里是从路由获取的数据----->{{this.$route.params.id}}</div>
-    <div class="fs30r g1890ff">优点----->url路径干净，没有乱七八糟的参数</div>
-    <div class="fs30r gred pt30r pb30r">缺点----->刷新数据丢失</div>
+  <div class="auto fs14 h100vh fs50r">
+    <div class="fs15 pt15 pb15 pl15">这里是从路由获取的数据----->{{this.$route.params.id}}</div>
+    <div class="fs15 g1890ff">优点----->url路径干净，没有乱七八糟的参数</div>
+    <div class="fs15 gred pt15 pb15">缺点----->刷新数据丢失</div>
   </div>
 </template>
 <script>
--- src/views/vueAdvance/query1/index.vue
+++ src/views/vueAdvance/query1/index.vue
@@ -1,9 +1,9 @@
 
 <template>
-  <div class="auto fs20r h100vh fs50r">
-    <div class="fs30r pt30r pb30r pl30r">这里是从路由获取的数据---->{{this.$route.query.id}}</div>
-    <div class="fs30r g1890ff">优点---->页面刷新数据不会丢失</div>
-    <div class="fs30r gred pt30r pb30r">缺点---->问号方式传参，敏感信息不能这么玩</div>
+  <div class="auto fs14 h100vh">
+    <div class="fs15 pt30r pb15 pl15">这里是从路由获取的数据---->{{this.$route.query.id}}</div>
+    <div class="fs15 g1890ff">优点---->页面刷新数据不会丢失</div>
+    <div class="fs15 gred pt15 pb15">缺点---->问号方式传参，敏感信息不能这么玩</div>
   </div>
 </template>
 <script>

7d581135db38af1d835442f53c1e07f392fb3852
周爱桩 <840043348@qq.com>
2023-01-23 15:40:33 +0800

    函数节流

--- src/views/vueAdvance/vThrottie/index.vue
+++ src/views/vueAdvance/vThrottie/index.vue
@@ -4,7 +4,7 @@
     <!--v-throttie函数节流-->
     <div class="fs20 tc pt30">v-throttie函数节流</div>
     <div class="fs12 tc mb10">一段时间内首次触发时立即执行，此时间段内再次触发，不会执行！</div>
-    <div class="fs16 gf bg1890ff rds40 f ac xc pt10 pb10 w40 mb15 auto" v-throttie="throttieRefresh">函数节流&nbsp;&nbsp;{{throttieTimes}}</div>
+    <div class="fs16 gf bg1890ff rds40 f ac xc pt10 pb10 w40 mb15 auto poi none" v-throttie="throttieRefresh">函数节流&nbsp;&nbsp;{{throttieTimes}}</div>
   </div>
 </template>
 <script>

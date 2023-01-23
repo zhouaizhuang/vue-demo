d6da9aebc05811d0cb5bca78797fb0aa42483f4f
周爱桩 <840043348@qq.com>
2023-01-23 16:14:07 +0800

    插槽

--- src/views/vueAdvance/vSlot/index.vue
+++ src/views/vueAdvance/vSlot/index.vue
@@ -4,16 +4,16 @@
     <baseLayout>
       <!--插槽替换header部件-->
       <template v-slot:header="slopProps">
-        <div class="bdc pt15r pb15r">{{slopProps.user.name}}</div> <!--使用原始组件定义的数据，进行渲染-->
+        <div class="bdc pt10 pb10">{{slopProps.user.name}}</div> <!--使用原始组件定义的数据，进行渲染-->
       </template>
       <!--插槽替换默认部件-->
       <template v-slot:default>
-        <div class="dib bg1890ff rds40r pl40r pr40r pt20r pb20r gf">{{123123}}</div>
+        <div class="dib bg1890ff rds40 pl20 pr20 pt10 pb10 gf">{{123123}}</div>
       </template>
       <!--插槽替换footer部件-->
       <!-- <template #footer="{info}"> 等价-->
       <template v-slot:footer="{info}"> <!--传值的时候可以使用es6的解构-->
-        <div class="g42b983 fs30r b pt20r pb20r">{{info.age}}</div>
+        <div class="g42b983 fs15 b pt10 pb10">{{info.age}}</div>
       </template>
     </baseLayout>
   </div>

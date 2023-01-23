6d82cd7a63ca5f773b70a7bbe4a4c24328c03b54
周爱桩 <840043348@qq.com>
2023-01-23 16:06:49 +0800

    vuex

--- src/views/vueAdvance/useVuex/index.vue
+++ src/views/vueAdvance/useVuex/index.vue
@@ -1,29 +1,29 @@
 
 <template>
-  <div class="auto fs20r h100vh fs50r">
+  <div class="auto fs14 h100vh">
     <!--使用方式一-->
-    <div class="fs40r b">使用方式一</div>
-    <div class="fs30r pt30r pl30r mb20r">{{optCount}}</div>
-    <div class="fs30r pt30r pl30r mb20r">{{count}}</div>
-    <div class="f xb pl30r pr30r">
-      <div class="fs30r f ac xc w30 bg1890ff rds40r gf pt20r pb20r" @click="increase">+1</div>
-      <div class="fs30r f ac xc w30 bg1890ff rds40r gf pt20r pb20r" @click="substract(2)">-2</div>
-      <div class="fs30r f ac xc w30 bg1890ff rds40r gf pt20r pb20r" @click="substractAsync">异步-2</div>
+    <div class="fs20 b">使用方式一</div>
+    <div class="fs15 pt15 pl15 mb10">{{optCount}}</div>
+    <div class="fs15 pt15 pl15 mb10">{{count}}</div>
+    <div class="f xb pl15 pr15">
+      <div class="fs15 f ac xc w30 bg1890ff rds40 gf pt10 pb10 poi none" @click="increase">+1</div>
+      <div class="fs15 f ac xc w30 bg1890ff rds40 gf pt10 pb10 poi none" @click="substract(2)">-2</div>
+      <div class="fs15 f ac xc w30 bg1890ff rds40 gf pt10 pb10 poi none" @click="substractAsync">异步-2</div>
     </div>
     <!--使用方式二-->
-    <div class="fs40r b mt100r">使用方式二</div>
-    <div class="fs30r pt30r pl30r mb20r">{{$store.getters.optCount}}</div>
-    <div class="fs30r pt30r pl30r mb20r">{{$store.state.count}}</div>
-    <div class="f xb pl30r pr30r">
-      <div class="fs30r f ac xc w30 bg1890ff rds40r gf pt20r pb20r" @click="add">+1</div>
-      <div class="fs30r f ac xc w30 bg1890ff rds40r gf pt20r pb20r" @click="remove">-2</div>
-      <div class="fs30r f ac xc w30 bg1890ff rds40r gf pt20r pb20r" @click="removeAsync">异步-2</div>
+    <div class="fs20 b mt50">使用方式二</div>
+    <div class="fs15 pt15 pl15 mb10">{{$store.getters.optCount}}</div>
+    <div class="fs15 pt15 pl15 mb10">{{$store.state.count}}</div>
+    <div class="f xb pl15 pr15">
+      <div class="fs15 f ac xc w30 bg1890ff rds40 gf pt10 pb10 poi none" @click="add">+1</div>
+      <div class="fs15 f ac xc w30 bg1890ff rds40 gf pt10 pb10 poi none" @click="remove">-2</div>
+      <div class="fs15 f ac xc w30 bg1890ff rds40 gf pt10 pb10 poi none" @click="removeAsync">异步-2</div>
     </div>
     <!--解决vuex刷新数据丢失问题-->
-    <div class="fs40r b mt100r">解决vuex刷新数据丢失问题</div>
-    <div class="fs30r f ac xc w40 bg1890ff rds40r gf pt20r pb20r" @click="getUserInfo">点击获取用户信息</div>
-    <div class="fs30r pt30r pl30r mb20r">用户名--->{{$store.state.userInfo.name }}</div>
-    <div class="fs30r pt30r pl30r mb20r">年龄--->{{$store.state.userInfo.age }}</div>
+    <div class="fs20 b mt50">解决vuex刷新数据丢失问题</div>
+    <div class="fs15 f ac xc w40 bg1890ff rds40 gf pt10 pb10" @click="getUserInfo">点击获取用户信息</div>
+    <div class="fs15 pt15 pl15 mb10">用户名--->{{$store.state.userInfo.name }}</div>
+    <div class="fs15 pt15 pl15 mb10">年龄--->{{$store.state.userInfo.age }}</div>
   </div>
 </template>
 <script>

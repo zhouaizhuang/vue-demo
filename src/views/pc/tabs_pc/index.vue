f82da8aaed6b617f84b1eb0d906f29d31c3bf89f
周爱桩 <840043348@qq.com>
2023-01-23 19:30:52 +0800

    优化tabs组件

--- src/views/pc/tabs_pc/index.vue
+++ src/views/pc/tabs_pc/index.vue
@@ -5,6 +5,7 @@
       <div class="f fs14 tc rel bdbe">
         <div @click="tabIndex=0" class="mr16 poi pt10 pb10" :style="{color: tabIndex == 0 ? '#2d8cf0' : '', width: '88px'}">标签1</div>
         <div @click="tabIndex=1" class="mr16 poi pt10 pb10" :style="{color: tabIndex == 1 ? '#2d8cf0' : '', width: '88px'}">标签2</div>
+        <div @click="tabIndex=2" class="mr16 poi pt10 pb10" :style="{color: tabIndex == 2 ? '#2d8cf0' : '', width: '88px'}">标签3</div>
         <div class="abs b0 trans3" :style="{backgroundColor:'#2d8cf0',width:'86px',height:'2px',transform: `translateX(${104 * tabIndex}px)`}"></div>
       </div>
       <div class="ovh rel fs14" style="height:450px;"> <!--高度必须限制（可以是固定值或者给定上下限-----width:clamp(200px, 50vh, 600px)）-->
@@ -13,6 +14,9 @@
         </div>
         <div :class="['trans3 abs trbl0 bgf pt10 pr10 pl0', tabIndex == 1 ? 'tx0' : tabIndex < 1 ? 'tx100' : 'tx-100']">
           我是标签2的内容
+        </div>
+        <div :class="['trans3 abs trbl0 bgf pt10 pr10 pl0', tabIndex == 2 ? 'tx0' : tabIndex < 2 ? 'tx100' : 'tx-100']">
+          我是标签3的内容
         </div>
       </div>
       <!--Tabs组件结束位置-->

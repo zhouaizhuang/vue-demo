ddae055c25bf922ea550a94a1d0eb454631ce66f
周爱桩 <840043348@qq.com>
2023-01-23 15:46:25 +0800

    调整样式

--- src/views/vueAdvance/propsEmit/father.vue
+++ src/views/vueAdvance/propsEmit/father.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="auto fs20r h100vh fs50r">
+  <div class="auto fs12 h100vh fs25">
     父组件
     <child :text="'我是字符串类型'" :msg="'我是来自父组件的数据'" :info="{name:'小红', school: '北京大学', age: 18}" :state="state" @sayHello="sayHello"/>
   </div>

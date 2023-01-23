0b20ee30e1c9c71e51e44f09c5d44ff3fdd9d32c
周爱桩 <840043348@qq.com>
2023-01-23 15:29:17 +0800

    权限控制

--- src/views/vueAdvance/vPermission/index.vue
+++ src/views/vueAdvance/vPermission/index.vue
@@ -1,29 +1,29 @@
 
 <template>
-  <div class="auto fs20r h100vh ovh">
+  <div class="auto fs20 h100vh ovh">
     <!--v-permission权限控制-->
-    <div class="fs40r tc mb20r pt30">v-permission权限控制</div>
+    <div class="fs26 tc mb20r pt30">v-permission权限控制</div>
     <div class="f ac pl50r mb30r">
-      <div class="fs30r mr20r b">首页权限:</div>
+      <div class="fs15r mr20r b">首页权限:</div>
       <div class="f1 f ac xs">
-        <div class="fs30r gf bg1890ff rds50r f ac xc pt20r pb20r w40" v-permission="'/index'">进入首页</div>
+        <div class="fs15 gf bg1890ff rds30 f ac xc pt10 pb10 w40" v-permission="'/index'">进入首页</div>
+      </div>
+    </div>
+    <div class="mt30 mb30 bg2" style="height:0.5px;"></div>
+
+    <div class="mb20r tl fs14 gred">备注：商机权限确实没有。所以这个dom页面上没有显示</div>
+    <div class="f ac pl25 mb15">
+      <div class="fs15 mr20 b">商机权限:</div>
+      <div class="f1 f ac xs">
+        <div class="fa15 gf bg1890ff rds50 f ac xc pt10 pb10 w40 " v-permission="'/saleChance'">进入商机</div>
       </div>
     </div>
     <div class="mt60r mb60r bg2" style="height:0.5px;"></div>
 
-    <div class="mb20r tl fs28r gred">备注：商机权限确实没有。所以这个dom页面上没有显示</div>
-    <div class="f ac pl50r mb30r">
-      <div class="fs30r mr20r b">商机权限:</div>
+    <div class="f ac pl25 mb15">
+      <div class="fs15 mr10 b">客户详情页权限:</div>
       <div class="f1 f ac xs">
-        <div class="fs30r gf bg1890ff rds50r f ac xc pt20r pb20r w40 " v-permission="'/saleChance'">进入商机</div>
-      </div>
-    </div>
-    <div class="mt60r mb60r bg2" style="height:0.5px;"></div>
-
-    <div class="f ac pl50r mb30r">
-      <div class="fs30r mr20r b">客户详情页权限:</div>
-      <div class="f1 f ac xs">
-        <div class="fs30r gf bg1890ff rds50r f ac xc pt20r pb20r w70 " v-permission="'/customer/detail'">进入客户详情页权限</div>
+        <div class="fs15 gf bg1890ff rds30 f ac xc pt10 pb10 w90 " v-permission="'/customer/detail'">进入客户详情页权限</div>
       </div>
     </div>
   </div>

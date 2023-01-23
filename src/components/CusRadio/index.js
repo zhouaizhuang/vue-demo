b0e861531437361a663fd66ba6852b8a6ac0b65d
周爱桩 <840043348@qq.com>
2023-01-23 16:54:08 +0800

    单选组件调整

---
+++ src/components/CusRadio/index.js
@@ -0,0 +1,22 @@
+export default {
+  name: 'cusRadio',
+  props: {
+    formInfo: {
+      type: Object, // 数据类型
+      require: true, // 必填
+      default: {}
+    },
+    type:{ // 当前数据是否是detail，也就是是否可以编辑
+      type: String, // 表单类型
+      default: ''
+    }
+  },
+  data(){
+    return {}
+  },
+  methods:{
+    
+  },
+  created(){
+  }
+}

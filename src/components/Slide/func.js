39e2d38c2bdc698953e19319bd9995f8f7ea08cb
周爱桩 <840043348@qq.com>
2023-02-11 14:13:26 +0800

    调整Y轴滚动距离，防止误操作

--- src/components/Slide/func.js
+++ src/components/Slide/func.js
@@ -10,22 +10,24 @@ export const clickItem = function () {
 //滑动开始
 export const touchStart = function(e) {
   // 记录初始位置
-  const list = searchCover(this.list, {id: this.item.id}, v => ({...v, startX: e.touches[0].clientX}))
+  const list = searchCover(this.list, {id: this.item.id}, v => ({...v, startX: e.touches[0].clientX, startY: e.touches[0].clientY}))
   this.$emit('setList', list)
 }
 //滑动结束
 export const touchEnd = function (e) {
-  const endX = e.changedTouches[0].clientX
-  const list = searchCover(this.list, {id: this.item.id}, v=> {
-    let slideType = v.slideType || 0
-    if(endX - v.startX > 30) { // 向右滑动距离超过30px， 判定为右滑
-      slideType++
-    } else if(endX - v.startX < -30){ // 向左滑动距离超过30px， 判定为左滑
-      slideType--
+  const { clientX: endX, clientY: endY } = e.changedTouches[0]
+  const list = searchCover(this.list, {id: this.item.id}, v => {
+    if(Math.abs(endY - v.startY) < 40) { // 上下滚动距离小于30
+      let slideType = v.slideType || 0
+      if(endX - v.startX > 30) { // 向右滑动距离超过30px， 判定为右滑
+        slideType++
+      } else if(endX - v.startX < -30){ // 向左滑动距离超过30px， 判定为左滑
+        slideType--
+      }
+      const min = this.type.includes('left') ? -1 : 0
+      const max = this.type.includes('right') ? 1 : 0
+      v.slideType = range(slideType, min, max)
     }
-    const min = this.type.includes('left') ? -1 : 0
-    const max = this.type.includes('right') ? 1 : 0
-    v.slideType = range(slideType, min, max)
     return v
   })
   this.$emit('setList', list)

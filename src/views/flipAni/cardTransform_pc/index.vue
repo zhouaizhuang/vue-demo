9e496ba0f3f042155829967a53830530b91bdb31
周爱桩 <840043348@qq.com>
2023-05-09 19:56:34 +0800

    增加注释

--- src/views/flipAni/cardTransform_pc/index.vue
+++ src/views/flipAni/cardTransform_pc/index.vue
@@ -1,6 +1,20 @@
 <template>
 <!-- https://juejin.cn/post/6844903772968206350 -->
 <!-- https://juejin.cn/post/7210324860647112761 -->
+
+<!-- 
+  在拖动目标上触发事件 (源元素):
+  ondragstart - 用户开始拖动元素时触发
+  ondrag - 元素正在拖动时触发
+  ondragend - 用户完成元素拖动后触发
+-->
+<!-- 
+  释放目标时触发的事件:
+  ondragenter - 当被鼠标拖动的对象进入其容器范围内时触发此事件
+  ondragover - 当某被拖动的对象在另一对象容器范围内拖动时触发此事件
+  ondragleave - 当被鼠标拖动的对象离开其容器范围内时触发此事件
+  ondrop - 在一个拖动过程中，释放鼠标键时触发此事件
+-->
   <div>
     <div class="f ac xc mb50">
       <Button @click="addItem" class="mr20" type="primary">添加</Button>

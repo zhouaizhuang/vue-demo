ff8d7a6a739d791bc7cdb604902a071b9415b213
周爱桩 <840043348@qq.com>
2023-06-16 13:32:30 +0800

    新增clickoutside.js

---
+++ src/directives/modules/clickoutside.js
@@ -0,0 +1,28 @@
+const clickoutsideContext = '@@clickoutsideContext'
+export const clickoutside = {
+  bind(el, binding, vnode) {
+    const documentHandler = (e) => {
+      if (vnode.context && el !== e.target && !el.contains(e.target)) {
+        vnode.context[el[clickoutsideContext].methodName]()
+      }
+    }
+    el[clickoutsideContext] = {
+      documentHandler,
+      methodName: binding.expression,
+      arg: binding.arg || 'click',
+    }
+    document.addEventListener(el[clickoutsideContext].arg, documentHandler, false)
+  },
+  update(el, binding) {
+    el[clickoutsideContext].methodName = binding.expression
+  },
+  unbind(el) {
+    document.removeEventListener(el[clickoutsideContext].arg, el[clickoutsideContext].documentHandler)
+  },
+  install(Vue) {
+    Vue.directive('clickoutside', {
+      bind: this.bind,
+      unbind: this.unbind,
+    })
+  }
+}

24e44cdc13a41729d12c47ca70e65025cc0748ed
周爱桩 <840043348@qq.com>
2023-07-01 21:09:58 +0800

    碰撞的小球效果

---
+++ src/views/css3Ani/ball/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="abs trbl0" style="background:#222;padding:500px 0 0;">
+    <div class="bgf auto rel rds10" style="width:340px;height:0;border-bottom:15px solid #fff;">
+      <div class="rds50 bgf abs b0  ball" style="width:60px;height:60px;left:42%;"></div>
+    </div>
+  </div>
+</template>
+<script src='./index.js'></script>
+<style src='./index.css' scoped></style>

c8303d27f46d679eb19d79c9389c05caa0bae6d4
周爱桩 <840043348@qq.com>
2023-09-27 10:01:49 +0800

    示例代码

--- src/views/vueAdvance/vPopTip/index.vue
+++ src/views/vueAdvance/vPopTip/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="abs trbl0">
-    <div class="f mb50 pl100 pt100">
+    <div class="mb50 pl100 pt100">
+      <!--外层盒子固定宽度，poptip自适应-->
       <div
         v-poptip="[
           {label:'设置标签', click: () => openSetTags(1)},
           {label:'快速签约', click: () => speedSign(2)},
           {label:'新增随访', click: () => addVisit(3)}
         ]"
-        class="tc poi"
+        class="tc poi bdc mb30"
         style="width:100px;"
+      >
+        更多操作
+      </div>
+
+      <!--外层盒子不设宽度， poptip设置宽度-->
+      <div
+        v-poptip="{
+          list: [
+            {label:'设置标签', click: () => openSetTags(1) },
+            {label:'快速签约', click: () => speedSign(2)},
+            {label:'新增随访', click: () => addVisit(3)}
+          ],
+        }"
+        class="tl poi bdc"
       >
         更多操作
       </div>

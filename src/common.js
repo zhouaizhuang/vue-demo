56dd1f3cf1f3486026efc3813345ccc1a97d4269
周爱桩 <840043348@qq.com>
2023-12-18 11:00:00 +0800

    完善注释

--- src/common.js
+++ src/common.js
@@ -359,7 +359,7 @@ export const uniqueObj = function (arr, field = isRequired(), type = 1) {
  *    {id: 1, name: '部门1', pid: 0},{id: 2, name: '部门2', pid: 1},{id: 3, name: '部门3', pid: 1},
  *    {id: 4, name: '部门4', pid: 3},{id: 5, name: '部门5', pid: 4}
  * ]
- * arrayToTree(arr)
+ * flat2tree(arr)
  * -----> 
  * [
  *   {

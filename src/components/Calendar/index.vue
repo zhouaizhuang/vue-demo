4950a86916610f75c65619b66ee6260ff73bc83d
周爱桩 <840043348@qq.com>
2024-03-28 01:15:59 +0800

    调整样式

--- src/components/Calendar/index.vue
+++ src/components/Calendar/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="mh100vh pl10 pr10 pt10">
+  <div>
     <div class="bgf">
       <div class="f ac xc pl10 pr10 pt10 pb10" style="border-bottom:1px solid #ccc;">
         <div @click="prevYear" class="mr25">{{'<<'}}</div>
         <div @click="prevMonth">{{'<'}}</div>
-        <div class="f1 f ac xc">{{year}}年{{ month}}月</div>
+        <div class="f1 f ac xc fs16">{{year}}年{{ month}}月</div>
         <div @click="nextMonth">{{'>'}}</div>
         <div @click="nextYear" class="ml25">{{'>>'}}</div>
       </div>
-      <div class="f ac fs16 mb5">
+      <div class="f ac fs14 mb5">
         <div class="w14 f ac xc">日</div>
         <div class="w14 f ac xc">一</div>
         <div class="w14 f ac xc">二</div>

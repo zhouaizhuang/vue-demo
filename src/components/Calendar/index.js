d43fba13bc5341e7b9bcfaf6baa6b6a5e1c06df9
周爱桩 <840043348@qq.com>
2024-03-28 01:27:40 +0800

    增加日历的默认值适配

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -21,13 +21,13 @@ export default {
   watch: {
     date: {
       async handler(newVal, oldVal) {
-        if(newVal) {
-          const {year, month} = _.socketTime(newVal)
-          this.year = year
-          this.month = month
-          const dateList = this.getDateArr(year, month)
-          this.dateList = dateList.map(v => ({...v, isChecked: _.dateFormater('YYYY-MM-DD', v.date) == _.dateFormater('YYYY-MM-DD', newVal)}))
-        }
+        // 没有传入值则默认选择今天
+        newVal = newVal ? newVal : _.dateFormater('YYYY-MM-DD', new Date())
+        const {year, month} = _.socketTime(newVal)
+        this.year = year
+        this.month = month
+        const dateList = this.getDateArr(year, month)
+        this.dateList = dateList.map(v => ({...v, isChecked: _.dateFormater('YYYY-MM-DD', v.date) == _.dateFormater('YYYY-MM-DD', newVal)}))
       },
       deep: true,
       immediate: true,

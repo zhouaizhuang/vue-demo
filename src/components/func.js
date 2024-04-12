6d20428549fc0b153d41aed3b1659c8464ab1646
周爱桩 <840043348@qq.com>
2024-04-12 14:49:57 +0800

    消息提示组件

--- src/components/func.js
+++ src/components/func.js
@@ -1,30 +1,71 @@
 import Vue from 'vue'
 import Confirm from '@/components/zConfirm/index.vue'
-const ConfirmConstructor = Vue.extend(Confirm)
-export const zConfirm = function ({title = '温馨提示', name = '',  content = '', okText='确定', cancelText='取消', onOk = () => {}, onCancel = () => {}} = {}) {
-  const confirm = new ConfirmConstructor()
-  confirm.$mount(document.createElement('div'))
-  confirm.isShow = true
-  confirm.title = title
-  confirm.name = name
-  confirm.content = content
-  confirm.okText = okText
-  confirm.cancelText = cancelText
-  confirm.onOk = () => {
-    onOk.call(this)
-    confirm.isShow=false
-    document.body.removeChild(confirm.$el)
+import Message from '@/components/zMessage/index.vue'
+/**
+ * 二次确认弹框使用方式
+  this.$zConfirm({
+    title: '温馨提示',
+    name: row.name,
+    content: `<p>确定要${row.state == 0 ? '启用' : '停用'}这个帐号吗？</p>`,
+    onOk: async () => {
+      await this.$post('/xxxChangeStatus', {id: row.id, status: toogle01(row.state)})
+      this.getList()
+    },
+    onCancel: () => {}
+  })
+ */
+export const zConfirm = (function (){
+  const ConfirmConstructor = Vue.extend(Confirm)
+  return function ({title = '温馨提示', name = '',  content = '', okText='确定', cancelText='取消', onOk = () => {}, onCancel = () => {}} = {}) {
+    const confirm = new ConfirmConstructor()
+    confirm.$mount(document.createElement('div'))
+    confirm.isShow = true
+    confirm.title = title
+    confirm.name = name
+    confirm.content = content
+    confirm.okText = okText
+    confirm.cancelText = cancelText
+    confirm.onOk = () => {
+      onOk.call(this)
+      confirm.isShow=false
+      document.body.removeChild(confirm.$el)
+    }
+    confirm.onCancel = () => {
+      onCancel.call(this)
+      confirm.isShow=false
+      document.body.removeChild(confirm.$el)
+    }
+    document.body.appendChild(confirm.$el)
   }
-  confirm.onCancel = () => {
-    onCancel.call(this)
-    confirm.isShow=false
-    document.body.removeChild(confirm.$el)
+})()
+/**
+ * 提示消息弹框
+ * this.$zMessage('请输入XXX', 'error') // 异常提示
+ * this.$zMessage('请输入XXX', 'warning') // 警告提示
+ * this.$zMessage('请输入XXX', 'info') // 消息提示
+ * this.$zMessage('请输入XXX', 'success') // 成功提示
+ */
+export const zMessage = (function () {
+  const MessageConstructor = Vue.extend(Message)
+  let messageIndex = 0
+  return function (msg = '', type = 'error'){
+    const message = new MessageConstructor()
+    message.$mount(document.createElement('div'))
+    message.top = messageIndex * 50 + 30
+    message.message = msg
+    message.type = type
+    document.body.appendChild(message.$el)
+    messageIndex++
+    setTimeout(() => {
+      message.isShow = false
+      messageIndex--
+    }, 2000)
   }
-  document.body.appendChild(confirm.$el)
-}
+})()
 // 注册到全局vue
 export default {
   install (Vue) {
     Vue.prototype.$zConfirm = zConfirm
+    Vue.prototype.$zMessage = zMessage
   }
 }
---
+++ src/components/zMessage/index.js
@@ -0,0 +1,23 @@
+import * as func from "./func.js"
+export default {
+  name: '',
+  components:{},
+  data(){
+    return {
+      messageList: [],
+      isShow: true,
+      top: 10,
+      message:'',
+      type:'error',
+    }
+  },
+  methods:{
+    ...func,
+  },
+  created(){
+    
+  },
+  mounted(){
+    
+  },
+}

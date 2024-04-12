05c599ae4bb8465fa3418b61280d27af08bb20c3
周爱桩 <840043348@qq.com>
2024-04-12 11:09:01 +0800

    增加zconfirm

--- src/components/func.js
+++ src/components/func.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import Confirm from '@/components/zConfirm/index.vue'
 const ConfirmConstructor = Vue.extend(Confirm)
-export const zConfirm = function ({title = '温馨提示', name = '',  content = '', onOk = () => {}, onCancel = () => {}} = {}) {
+export const zConfirm = function ({title = '温馨提示', name = '',  content = '', okText='确定', cancelText='取消', onOk = () => {}, onCancel = () => {}} = {}) {
   const confirm = new ConfirmConstructor()
   confirm.$mount(document.createElement('div'))
   confirm.isShow = true
   confirm.title = title
   confirm.name = name
   confirm.content = content
+  confirm.okText = okText
+  confirm.cancelText = cancelText
   confirm.onOk = () => {
     onOk.call(this)
     confirm.isShow=false
-    setTimeout(() => { document.body.removeChild(confirm.$el) }, 600)
+    document.body.removeChild(confirm.$el)
   }
   confirm.onCancel = () => {
     onCancel.call(this)
     confirm.isShow=false
-    setTimeout(() => { document.body.removeChild(confirm.$el) }, 600)
+    document.body.removeChild(confirm.$el)
   }
   document.body.appendChild(confirm.$el)
 }
